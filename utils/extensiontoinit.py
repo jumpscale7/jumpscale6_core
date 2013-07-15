48f4a0d7684c5af9bc86e732cb3048d92cfe0169
Jo De Boeck <deboeck.jo@gmail.com>
2013-07-15 12:57:29 +0200

    Make extensiontoinit generate cleaner code

--- utils/extensiontoinit.py
+++ utils/extensiontoinit.py
@@ -12,18 +12,21 @@ o.%(location)s.%(hook)s = %(classname)s()
 
 cfg = ConfigParser()
 cfg.read('extension.cfg')
-output = ["from OpenWizzy import o"]
+imports = ["from OpenWizzy import o"]
+available = set()
+data = list()
 for section in cfg.sections():
     location = cfg.get(section, 'qlocation')
     classname = cfg.get(section, 'classname')
     modulename = cfg.get(section, 'modulename')
     locationparts = location.split(".")
-    output.insert(1, "from .%s import %s" % (modulename, classname) )
-    output.append("o.base.loader.makeAvailable(o, '%s')" % ".".join(locationparts[1:-1] ))
-    output.append("o.%s = %s()" % (".".join(locationparts[1:]), classname))
+    imports.append("from .%s import %s" % (modulename, classname) )
+    available.add(".".join(locationparts[1:-1]))
+    data.append("o.%s = %s()" % (".".join(locationparts[1:]), classname))
 
-output.append("")
+data.append("")
+result = imports + [ "o.base.loader.makeAvailable(o, '%s')" % x for x in available ] + data
 with open('__init__.py', 'w') as fd:
-    fd.write("\n".join(output))
+    fd.write("\n".join(result))
 
 
