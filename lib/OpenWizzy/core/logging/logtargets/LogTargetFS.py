a1f1e07c8e28d7c01ed26215a0a2412986d1c195
Jo De Boeck <deboeck.jo@gmail.com>
2013-07-22 20:11:46 +0200

    Make LogTargetFS behave more like other loggers eg. nginx and apache...

--- lib/OpenWizzy/core/logging/logtargets/LogTargetFS.py
+++ lib/OpenWizzy/core/logging/logtargets/LogTargetFS.py
@@ -1,13 +1,16 @@
 from OpenWizzy import o
 import time
 from datetime import datetime
-import sys,os
-#import sitecustomize
+import sys
+import os
+# import sitecustomize
 import random
 
 #@todo time is not local time, redo
 
+
 class LogTargetFS(object):
+
     """
     log to local filesystem
     """
@@ -16,36 +19,30 @@ class LogTargetFS(object):
         """
         """
         self.enabled = False
-        #cannot use openwizzy primitives yet, not enabled yet
+        # cannot use openwizzy primitives yet, not enabled yet
         self.name = "file"
         self.fileHandle = None
-        self.logopenTime=self._gettime()
-        self.logopenNrlines=0
-        self.logfile=""
+        self.logopenTime = self._gettime()
+        self.logopenNrlines = 0
+        self.logfile = ""
         self._initializing = False
-        self.agentid=o.application.agentid
-        self.appname=o.application.appname.split(':')[0]
-        self.lastappstatus=o.application.state
-        #except:
-            #self.agentid="unknown"
+        self.agentid = o.application.agentid
+        self.appname = o.application.appname.split(':')[0]
+        self.lastappstatus = o.application.state
+        self._queue = []
+        # except:
+            # self.agentid="unknown"
         if o.system.platformtype.isWindows():
-            logdir=os.path.join(o.system.windows.getTmpPath(),"QBASEVAR")
+            logdir = os.path.join(o.system.windows.getTmpPath(), "QBASEVAR")
             if not os.path.isdir(logdir):
                 os.mkdir(logdir)
-            logdir=os.path.join(logdir,"LOGSTARTUP")
+            logdir = os.path.join(logdir, "LOGSTARTUP")
             if not os.path.isdir(logdir):
                 os.mkdir(logdir)
-        else:
-            logdir=os.path.join(o.dirs.baseDir,"var",'log',"openwizzylogs")
-        
-        if not os.path.isdir(logdir):
-            os.mkdir(logdir)
-        if os.path.isdir(logdir)==False:
-            os.mkdir(logdir)
-            
-        self.enabled = True #self.checkTarget()
 
-        #o.base.time.getLocalTimeHRForFilesystem()
+        self.enabled = True  # self.checkTarget()
+
+        # o.base.time.getLocalTimeHRForFilesystem()
 
     def _gettime(self):
         return int(time.time())
@@ -59,43 +56,47 @@ class LogTargetFS(object):
             return self.open()
         return True
 
-    def log(self, log):
+    def log(self, log, skipQueue=False):
         """
         """
         if not self._is_initialized():
-            skip=self._initialize()
+            skip = self._initialize()
             if skip:
+                self._queue.append(log)
                 return
-        
+        if not skipQueue:
+            for queitem in self._queue:
+                self.log(queitem, True)
+            self._queue = list()
+
         if not self.enabled:
             self.enabled = self.checkTarget()
 
-        ttime=time.strftime("%H:%M:%S: ", datetime.fromtimestamp(log.epoch).timetuple())
-        message="%s %s %s%s" % (log.level, o.application.appname , ttime, log.message)
+        ttime = time.strftime("%H:%M:%S: ", datetime.fromtimestamp(log.epoch).timetuple())
+        message = "%s %s %s%s" % (log.level, o.application.appname, ttime, log.message)
 
         appLogname = o.application.appname.split(':')[0]
 
-        ##print self._gettime()>(self.logopenTime+60)
+        # print self._gettime()>(self.logopenTime+60)
         if self._config['main']['logrotate_enable'] == 'True':
             if self._gettime() > (self.logopenTime + int(self._config['main']['logrotate_time'])) \
                 or self.logopenNrlines > int(self._config['main']['logrotate_number_of_lines']) \
                 or self.appname <> appLogname \
-                or o.application.state <> self.lastappstatus:
-                
-                ##print "NEWLOG"
-                self.close()
-                self.open()
-                self.logopenTime=self._gettime()
-                self.logopenNrlines=0
-                self.lastappstatus=o.application.state
+                    or o.application.state <> self.lastappstatus:
+
+                # print "NEWLOG"
+                self.rotate()
+                self.logopenTime = self._gettime()
+                self.logopenNrlines = 0
+                self.lastappstatus = o.application.state
 
         try:
-            #print "log:%s" % message
-            self.fileHandle.write("%s\n"%message)
+            # print "log:%s" % message
+            self.fileHandle.write("%s\n" % message)
             self.fileHandle.flush()
         except:
             self.enabled = False
-            ##print "LOG:%s" %message
+            # print "LOG:%s" %message
             return False
 
         return True
@@ -105,37 +106,39 @@ class LogTargetFS(object):
             return False
         if not isinstance(other, LogTargetFS):
             return False
-
         return True
 
     def open(self):
-        
         if not self._is_initialized():
             self._initialize()
 
         appLogname = o.application.appname.split(':')[0]
-        logdir=os.path.join(o.dirs.varDir,'log',"openwizzylogs",appLogname)
 
-        if appLogname<>self.appname:
-            self.appname=appLogname
+        if appLogname <> self.appname:
+            self.appname = appLogname
 
-        if not os.path.isdir(logdir):
-            os.makedirs(logdir)
-        filename=time.strftime("%b_%d--%H_%M_%S", time.gmtime())
-        logfile=os.path.join(logdir,"%s_%s"%(filename,o.application.state))
-        filenum = 1
-        if os.path.isfile("%s.log" % logfile):
-            logfile = "%s_%s"%(logfile, random.randint(1,10000))
-            filenum += 1
-
-        self.logfile="%s.log" % logfile
-        #print "logfile:%s" % self.logfile
+        if not os.path.isdir(o.dirs.logDir):
+            os.makedirs(o.log.logDir, 0755)
+        logfile = os.path.join(o.dirs.logDir, appLogname)
+        self.logfile = "%s.log" % logfile
+        # print "logfile:%s" % self.logfile
         try:
             self.fileHandle = open(self.logfile, 'a')
         except:
             return False
-
         return True
+
+    def rotate(self):
+        cnt = 1
+        while True:
+            filename = os.path.join(o.dirs.logDir, "%s.log.%d" % (self.appname, cnt))
+            if not o.system.fs.exists(filename):
+                break
+            cnt += 1
+        if self.logfile and os.path.exists(self.logfile):
+            os.rename(self.logfile, filename)
+        self.close()
+        self.open()
 
     def close(self):
         if self.fileHandle:
@@ -169,37 +172,36 @@ class LogTargetFS(object):
                         'Unable to close log file after %d attempts.\n'
                         % (max_attempts,))
             self.fileHandle = None
-            
+
     def cleanup(self):
-        
+
         if self.__dict__.has_key("_config"):
             if self._config['main']['logremove_enable'] == 'True':
-              
+
                 if int(self._lastcleanuptime) <= (o.base.time.getTimeEpoch() - int(self._config['main']['logremove_check'])):
-            
-                    self._lastcleanuptime=o.base.time.getTimeEpoch()
-                    self.nolog=True
-                    inifile=o.config.getInifile("main")
-                    inifile.setParam("main","lastlogcleanup",self._lastcleanuptime)
-                    files= o.system.fs.listFilesInDir( \
-                                o.system.fs.joinPaths(o.dirs.logDir, 'openwizzylogs'), \
-                                recursive=True, \
-                                maxmtime=(o.base.time.getTimeEpoch() - int(self._config['main']['logremove_age'])))
-                    
+
+                    self._lastcleanuptime = o.base.time.getTimeEpoch()
+                    self.nolog = True
+                    inifile = o.config.getInifile("main")
+                    inifile.setParam("main", "lastlogcleanup", self._lastcleanuptime)
+                    files = o.system.fs.listFilesInDir(
+                        o.system.fs.joinPaths(o.dirs.logDir, 'openwizzylogs'),
+                        recursive=True,
+                        maxmtime=(o.base.time.getTimeEpoch() - int(self._config['main']['logremove_age'])))
+
                     for filepath in files:
                         if o.system.fs.exists(filepath):
                             try:
                                 o.system.fs.removeFile(filepath)
                             except Exception, ex:
-                                pass # We don't want to fail on logging
-                
-                    self.nolog=False
-                
+                                pass  # We don't want to fail on logging
+
+                    self.nolog = False
+
     def _is_initialized(self):
         return hasattr(self, '_config') and self._config and hasattr(self, '_lastcleanuptime')
-    
+
     def _initialize(self):
-        
         """
         Initialize logtarget config
 
@@ -209,58 +211,48 @@ class LogTargetFS(object):
         if o.application.state != o.enumerators.AppStatusType.RUNNING or self._initializing:
             return True
         self._initializing = True
-        
+
         mainfile = o.config.getInifile("main")
         if 'main' in mainfile.getSections() and mainfile.getValue('main', 'lastlogcleanup'):
             self._lastcleanuptime = mainfile.getValue('main', 'lastlogcleanup')
         else:
             self._lastcleanuptime = -1
- 
+
         # Check if we should rotate logfiles
-        """
-        In [6]: o.config.getConfig('logtargetfs')                                                                                                                                                     
-        {'main': {'logremove_enable': 'True',
-                  'logrotate_enable': 'True',
-                  'logrotate_number_of_lines': '5000',
-                  'logremove_age': '432000',
-                  'logrotate_time': '60',
-                  'logremove_check': '86400'}}
-        """
-        
+
         cfg_defaults = {'main': {'logremove_enable': 'True',
-                  'logrotate_enable': 'True',
-                  'logrotate_number_of_lines': '5000',
-                  'logremove_age': '432000',
-                  'logrotate_time': '60',
-                  'logremove_check': '86400'}}
-        
+                                 'logrotate_enable': 'True',
+                                 'logrotate_number_of_lines': '5000',
+                                 'logremove_age': '432000',
+                                 'logrotate_time': '60',
+                                 'logremove_check': '86400'}}
+
         cfg = None
         if 'logtargetfs' in o.config.list():
             logtargetfs_file = o.config.getInifile("logtargetfs")
             cfg = logtargetfs_file.getFileAsDict()
-        
+
         if not cfg or not 'main' in cfg.keys():
             cfg = cfg_defaults
-            
+
         self._config = cfg
-        
+
         for k, v in cfg_defaults['main'].iteritems():
             if not self._config['main'].has_key(k) or self._config['main'].get(k) == None:
                 self._config['main'][k] = v
 
-
         self.enabled = self.checkTarget()
         self._initializing = False
 
-        
+
 # Config
-#from OpenWizzy.core.config import ConfigManagementItem, ItemSingleClass
+# from OpenWizzy.core.config import ConfigManagementItem, ItemSingleClass
 #
-#class LogTargetFSConfigManagementItem(ConfigManagementItem):
+# class LogTargetFSConfigManagementItem(ConfigManagementItem):
 #    """
 #    Configuration of a Cloud API connection
 #    """
-#    # (MANDATORY) CONFIGTYPE and DESCRIPTION
+# (MANDATORY) CONFIGTYPE and DESCRIPTION
 #    CONFIGTYPE = "logtargetfs"
 #    DESCRIPTION = "Pylabs Filesystem Logtarget"
 #    KEYS ={"logrotate_enable":"",
@@ -268,9 +260,9 @@ class LogTargetFS(object):
 #           "logrotate_time":"",
 #           "logremove_enable":"",
 #           "logremove_age":"",
-#           "logremove_check":""           
+#           "logremove_check":""
 #           }
-#    # MANDATORY IMPLEMENTATION OF ASK METHOD
+# MANDATORY IMPLEMENTATION OF ASK METHOD
 #    def ask(self):
 #        self.dialogAskYesNo('logrotate_enable', 'Enable automatic rotation of logfiles', True)
 #        if self.params['logrotate_enable']:
@@ -280,21 +272,21 @@ class LogTargetFS(object):
 #        if self.params['logremove_enable']:
 #            self.dialogAskInteger('logremove_age', 'Max age of logfiles in seconds', 432000)
 #            self.dialogAskInteger('logremove_check', 'Interval to remove files older than max age', 86400)
-#            
-#            
-#    #  OPTIONAL CUSTOMIZATIONS OF CONFIGURATION
+#
+#
+# OPTIONAL CUSTOMIZATIONS OF CONFIGURATION
 #
 #    def show(self):
 #        """
 #        Optional customization of show() method
 #        """
-#        # Here we do not want to show the password, so a customized show() method
+# Here we do not want to show the password, so a customized show() method
 #        o.gui.dialog.message(self.params)
-#        
+#
 #    def retrieve(self):
 #        """
 #        Optional implementation of retrieve() method, to be used by find()
 #        """
 #        return self.params
 #
-#      
+#
