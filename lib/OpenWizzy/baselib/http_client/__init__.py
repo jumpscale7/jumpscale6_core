74c65ec14b147526cfddd29eb81f7e7befb7b131
Jo De Boeck <deboeck.jo@gmail.com>
2013-07-25 07:56:21 +0200

    Change __init__ file

--- lib/OpenWizzy/baselib/http_client/__init__.py
+++ lib/OpenWizzy/baselib/http_client/__init__.py
@@ -1,12 +1,4 @@
 from OpenWizzy import o
-from HttpClient import HttpClient 
-
-class Empty():
-    pass
-
-if not  o.__dict__.has_key("clients"):
-    o.clients=Empty()
-
-o.clients.http=HttpClient()
-
-
+from .HttpClient import HttpClient
+o.base.loader.makeAvailable(o, 'clients')
+o.clients.http = HttpClient()
