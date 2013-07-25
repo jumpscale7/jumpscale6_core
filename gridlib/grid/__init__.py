3809da8ced6003095aa9e9c049be5539b68dbf86
Sherif Zain <zains@codescalers.com>
2013-07-25 10:07:27 +0200

    Converted grid __init__

--- gridlib/grid/__init__.py
+++ gridlib/grid/__init__.py
@@ -1,15 +1,4 @@
 from OpenWizzy import o
-import OpenWizzy.baselib.serializers
-from GridFactory import GridFactory
-
-class Empty():
-	pass
-
-class Empty():
-	pass
-
-if not  o.__dict__.has_key("core"):
-	o.core=Empty()
-
-o.core.grid=GridFactory()
-
+from .GridFactory import GridFactory
+o.base.loader.makeAvailable(o, 'core')
+o.core.grid = GridFactory()
