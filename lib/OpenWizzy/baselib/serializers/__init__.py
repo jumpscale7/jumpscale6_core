417859410d685c2274173f617ec8c3ba84ee125a
Jo De Boeck <deboeck.jo@gmail.com>
2013-08-05 14:26:13 +0200

    Fix syntax error of bad merge

--- lib/OpenWizzy/baselib/serializers/__init__.py
+++ lib/OpenWizzy/baselib/serializers/__init__.py
@@ -3,19 +3,12 @@ from .SerializerInt import SerializerInt
 from .SerializerTime import SerializerTime
 from .SerializerBase64 import SerializerBase64
 from .SerializerHRD import SerializerHRD
-
-from SerializersFactory import SerializersFactory
+from .SerializersFactory import SerializersFactory
 
 o.base.loader.makeAvailable(o, 'db')
 
 o.db.serializers=SerializersFactory()
-
 o.db.serializers.int = SerializerInt()
 o.db.serializers.time = SerializerTime()
 o.db.serializers.base64 = SerializerBase64()
-<<<<<<< local
-
-
-=======
 o.db.serializers.hrd = SerializerHRD()
->>>>>>> other
