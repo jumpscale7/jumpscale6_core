bb713ae9a6572e18f76f4b1bda7c2d5557eb63b9
Kristof De Spiegeleer <kristof@incubaid.com>
2013-08-10 13:58:29 +0200

    logger

--- apps/osis/logic/broker_17/OSIS_category_template.py
+++
@@ -1,9 +0,0 @@
-from OpenWizzy import o
-
-parentclass=o.core.osis.getOsisImplementationParentClass("_coreobjects")  #is the name of the namespace
-
-class mainclass(parentclass):
-    """
-    """
-
-
