585637fd9f96d79ffe65686c82becdd88ae79b78
Jo De Boeck <deboeck.jo@gmail.com>
2013-08-14 14:36:51 +0200

    Cleanup install make baseinstall use the same code as owextend

--- lib/OpenWizzy/baselib/owdeveltools/OWDevelToolsInstaller.py
+++ lib/OpenWizzy/baselib/owdeveltools/OWDevelToolsInstaller.py
@@ -43,23 +43,22 @@ class OWDevelToolsInstaller:
         o.system.platform.ubuntu.check()
         do=o.system.installtools
 
-        do.execute("apt-get update",dieOnNonZeroExitCode=False)
+        print "Updating metadata"
+        o.system.platform.ubuntu.updatePackageMetadata()
 
         debpackages = ('python2.7','nginx', 'curl', 'mc', 'ssh', 'mercurial', 'python-gevent', 'python-simplejson', 'python-numpy',
                         'byobu', 'python-apt','ipython','python-pip','python-imaging','python-requests',"python-paramiko","gcc",
-                        "g++","python-dev","msgpack-python","python-mhash","python-snappy","python-beaker","python-mimeparse","python-m2crypto",
+                        "g++","python-dev","python-mhash","python-snappy","python-beaker","python-mimeparse","python-m2crypto",
                         "openjdk-7-jre")
 
         for name in debpackages:
             print "check install %s"%name
             o.system.platform.ubuntu.install(name)
 
-        do.execute("apt-get purge python-zmq -y")
-        do.execute("pip install 'pyzmq==13.0.2'")
-        
-        pypackages = ('urllib3', 'ujson', 'blosc', 'pylzma','circus')
-        for package in pypackages:
-            do.execute("easy_install %s" % package)
+        o.system.platform.ubuntu.remove('python-zmq')
+        pypackages = ('urllib3', 'ujson', 'blosc', 'pylzma','circus', 'msgpack-python>=0.3.0', 'pyzmq==13.0.2')
+        pypackages = [ '"%s"' % x for x in pypackages ]
+        do.execute("pip install %s" % ' '.join(pypackages))
 
     def deployExampleCode(self):
         """
--- utils/baseinstallFromRepo.py
+++ utils/baseinstallFromRepo.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.insert(0,'../lib')
 
-from OpenWizzy import *
+from OpenWizzy import o
 
 import OpenWizzy.baselib.platforms #gets access to ubuntu code
 import OpenWizzy.baselib.owdeveltools
@@ -12,53 +12,9 @@ o.application.start("owinstaller",basedir="/opt/openwizzy6/",appdir="/opt/openwi
 
 if o.system.platformtype.isLinux():
     o.system.platform.ubuntu.check()
-
-
-    def installpackages():
-        do.execute("apt-get update")
-
-    #     result=self.execute("apt-get install nginx curl mc ssh mercurial byobu python-gevent python-simplejson python-numpy redis-server python-pycryptopp -y")
-    #     result=self.execute("apt-get install msgpack-python python-pip python-dev python-zmq -y")
-        
-        # do.execute("apt-get upgrade -y")
-        debpackages = ('python2.7','nginx', 'curl', 'mc', 'ssh', 'mercurial', 'python-gevent', 'python-simplejson', 'python-numpy',
-                        'byobu', 'python-apt','ipython','python-pip','python-imaging','python-requests',"python-paramiko","gcc","g++","python-dev","python-zmq","msgpack-python","python-mhash","python-snappy") 
-        for name in debpackages:
-            print "check install %s"%name
-            o.system.platform.ubuntu.install(name)
-
-        pypackages = ('urllib3', 'ujson', 'blosc', 'pylzma')
-        for package in pypackages:
-            do.execute("easy_install %s" % package)
-
-    installpackages()
-
-    # pythpath="/usr/lib/python2.7/"
-    # if not o.system.fs.exists(pythpath):
-    #     raise RuntimeError("Could not find python 2.7 env on %s"%pythpath)
-
+    o.develtools.installer.preparePlatformUbuntu()
     o.develtools.installer.link2code()
 
-    # owdir="%s/OpenWizzy"%pythpath
-    # do.createdir(owdir)
-
-    # do.copydeletefirst("%s/__init__.py"%o.dirs.libDir,"%s/__init__.py"%owdir)
-    # srcdir=o.dirs.libDir
-    # for item in ["base","baselib","core"]:
-    #     do.symlink("%s/%s"%(srcdir,item),"%s/%s"%(owdir,item))  
-
-    # do.createdir("%s/%s"%(o.dirs.baseDir,"apps"))
-
-    # src="%s/../../shellcmds"%o.dirs.libDir
-    # dest="%s/shellcmds"%o.dirs.baseDir
-    # do.symlink(src,dest)  
-
-    # for item in o.system.fs.listFilesInDir(dest,filter="ow*.py"):
-    #     C="python %s/%s $@"%(dest,o.system.fs.getBaseName(item))
-    #     path="/usr/bin/%s"%o.system.fs.getBaseName(item).replace(".py","")
-    #     o.system.fs.writeFile(path,C)
-    #     cmd='chmod 777 %s'%path
-    #     o.system.process.execute(cmd)
 
 else:
     raise RuntimeError("Openwizzy 6 is for now only supported on ubuntu or mint.")
