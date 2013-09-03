f190489762b8eac0e19b79a607787763fcba6bad
Rob Van Mieghem <rob@vanmieghemcloud.com>
2013-09-03 11:06:20 +0200

    Make sure 'JumpScale' can act as a namespace package

--- lib/JumpScale/__init__.py
+++ lib/JumpScale/__init__.py
@@ -1,6 +1,12 @@
 __version__ = '6.0.0'
 __all__ = ['__version__', 'j']
 
+import pkgutil
+__path__ = pkgutil.extend_path(__path__, __name__)
+del pkgutil
+
+
+
 class JumpScale():
 	def __init__(self):
 		pass
