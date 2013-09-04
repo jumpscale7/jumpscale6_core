55b3ee9c0bfa1de374992a361c1d75638be2d789
Reem Khamis <khamisr@codescalers.com>
2013-09-04 17:47:10 +0200

    Pylabs --> JS. Extension needs fixing

--- apps/portalbase/portal_client.py
+++ apps/portalbase/portal_client.py
@@ -1,16 +1,9 @@
 import time
-from pylabs.InitBase import *
+from JumpScale import j
 
-q.application.appname = "appserver6_client"
-q.application.start()
+j.application.start("appserver6_client")
 
-client = q.core.appserver6.getAppserverClient("127.0.0.1", 9999, "1234")
+client = j.core.appserver6.getAppserverClient("127.0.0.1", 9999, "1234")
 system = client.getActor("system", "master", instance=0)
 
-
-from pylabs.Shell import ipshell
-print "DEBUG NOW main"
-ipshell()
-
-
-q.application.stop()
+j.application.stop()
