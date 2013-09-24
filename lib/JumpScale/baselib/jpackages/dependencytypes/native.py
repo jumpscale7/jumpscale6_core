606e1ab77f781abea0a29737a136e54a809786a6
Rob Van Mieghem <rob@vanmieghemcloud.com>
2013-09-24 13:07:37 +0200

    Merge

--- lib/JumpScale/baselib/jpackages/dependencytypes/native.py
+++
@@ -1,7 +0,0 @@
-from JumpScale import j
-from . import AbstractPackage
-
-class Package(AbstractPackage):
-    def install(self, *args, **kwargs):
-         j.system.platform.ubuntu.install(self.name)
-
