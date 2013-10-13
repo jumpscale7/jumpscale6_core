214b2e4b65f366958987e637527d1e9f240e9ff5
Jo De Boeck <deboeck.jo@gmail.com>
2013-10-13 16:29:10 +0200

    Move the way we add the css style to jpackageslist
    Reviewer: Sherif Zain

--- apps/portalbase/system/system__contentmanager/macros/page/jpackageslist/3_main.py
+++ apps/portalbase/system/system__contentmanager/macros/page/jpackageslist/3_main.py
@@ -1,14 +1,14 @@
 def main(j, args, params, tags, tasklet):
 
     page = args.page
-
+    attributes = "class='nav nav-list' style='-moz-column-count: 3; -webkit-column-count:3; column-count:3;'"
     def _getDomainPackages(d):
         page.addHeading(d.domainname, 2)
         packages = sorted(d.getJPackages(), key=lambda x: x.name)
         for p in packages:
             href = '/system/JPackageShow?domain=%s&name=%s&version=%s' % (d.domainname, p.name, p.version)
             icon = 'icon-ok' if p.isInstalled() else 'icon-remove'
-            page.addBullet("<a href='%s'><i class='%s'></i> %s</a>" % (href, icon, p.name), attributes="class='nav nav-list'")
+            page.addBullet("<a href='%s'><i class='%s'></i> %s</a>" % (href, icon, p.name), attributes=attributes)
 
     domain = args.tags.tagGet('domain', '')
     if not domain:
