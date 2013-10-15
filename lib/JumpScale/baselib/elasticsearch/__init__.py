49b8f4f446484c34f909b95d84a88d40e832ad6d
Hendrik Demolder <hendrik.demolder@gmail.com>
2013-10-15 17:59:56 +0200

    Remove old elasticsearch manager, elasticsearch management is now trough circus

--- lib/JumpScale/baselib/elasticsearch/__init__.py
+++ lib/JumpScale/baselib/elasticsearch/__init__.py
@@ -1,7 +1,4 @@
 from JumpScale import j
 from .Elasticsearch import ElasticsearchFactory
-from .elasticsearchmanager import ElasticsearchManager
 j.base.loader.makeAvailable(j, 'clients')
-j.base.loader.makeAvailable(j, 'manage')
-j.manage.elasticsearch = ElasticsearchManager()
 j.clients.elasticsearch = ElasticsearchFactory()
--- lib/JumpScale/baselib/elasticsearch/elasticsearchmanager.py
+++
@@ -1,5 +0,0 @@
-from JumpScale.baselib.manage.managerbase import ManagerBase
-
-class ElasticsearchManager(ManagerBase):
-
-    _servicename = 'elasticsearch'
