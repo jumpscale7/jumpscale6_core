ce87f1f3c811df2a79cd0e97f215d5de62b0e5ba
Jo De Boeck <deboeck.jo@gmail.com>
2013-10-20 11:35:38 +0200

    Lesser evil of checking for mercurial

--- lib/JumpScale/baselib/mercurial/__init__.py
+++ lib/JumpScale/baselib/mercurial/__init__.py
@@ -3,4 +3,7 @@ from .HgLibFactory import HgLibFactory
 j.base.loader.makeAvailable(j, 'clients')
 j.clients.mercurial = HgLibFactory()
 
-j.system.platform.ubuntu.checkInstall("mercurial","hg")
+try:
+    import mercurial
+except ImportError:
+    j.system.platform.ubuntu.checkInstall("mercurial","hg")
