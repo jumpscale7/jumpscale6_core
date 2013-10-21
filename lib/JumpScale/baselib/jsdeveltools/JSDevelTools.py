839705063cc13c3c920330525742cc01d4420684
Sherif Zain <zains@codescalers.com>
2013-10-21 15:32:08 +0200

    No need to start es, its already started via circus in jpackage es configure

--- lib/JumpScale/baselib/jsdeveltools/JSDevelTools.py
+++ lib/JumpScale/baselib/jsdeveltools/JSDevelTools.py
@@ -26,14 +26,6 @@ class JSDevelTools:
 
         j.system.platform.screen.createSession(name,screens)
 
-        #start elastic search if needed
-        print '* Starting elasticsearch'
-        if not j.system.net.tcpPortConnectionTest("127.0.0.1",9200):
-            import JumpScale.baselib.circus
-            j.tools.circus.manager.startProcess('elasticsearch')
-            #wait for 30 sec on start
-            j.system.net.waitConnectionTest("127.0.0.1",9200,30)
-
         #start osis
         print '* Starting osis'
         path=j.system.fs.joinPaths("/opt/jumpscale/apps","osis")
