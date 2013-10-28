12cbeb8a628cc83d8025f68df6928f4c3a6c2b1a
Sherif Zain <zains@codescalers.com>
2013-10-28 14:42:39 +0200

    WIP: dnsmasq extension initial commit

---
+++ lib/JumpScale/baselib/dnsmasq/dnsmasq.py
@@ -0,0 +1,44 @@
+from JumpScale import j
+
+class DNSMasq(object):
+
+    def __init__(self, config_path=None):
+        if not config_path:
+            self._configfile = j.system.fs.joinPaths('/etc', 'dnsmasq.conf')
+        else:
+            self._configfile = config_path
+        if not j.system.platform.ubuntu.findPackagesInstalled('dnsmasq'):
+            j.system.platform.ubuntu.install('dnsmasq')
+
+    @property
+    def configpath(self):
+        return self._configfile
+
+    @configpath.setter
+    def configpath(self, value):
+        self._configfile = value
+
+    def addHost(self, macaddress, ipaddress, name=None):
+        """Adds a dhcp-host entry to dnsmasq.conf file"""
+        te = j.codetools.getTextFileEditor(self._configfile)
+        contents = 'dhcp-host=%s' % macaddress
+        if name:
+            contents += ',%s' % name
+        contents += ',%s\n' % ipaddress
+        te.appendReplaceLine('.*%s.*' % macaddress, contents)
+        te.save()
+        self.restart()
+
+    def removeHost(self, macaddress):
+        """Removes a dhcp-host entry from dnsmasq.conf file"""
+        te = j.codetools.getTextFileEditor(self._configfile)
+        te.deleteLines('.*%s.*' % macaddress)
+        te.save()
+        self.restart()
+
+    def restart(self):
+        """Restarts dnsmasq"""
+        j.system.process.execute('/etc/init.d/dnsmasq restart')
+
+    def reload(self):
+        pass
