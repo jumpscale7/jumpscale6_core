fa68b1849456d61950eb6b38087073521c3d70f2
Kristof De Spiegeleer <kristof@incubaid.com>
2013-11-05 10:37:08 +0100

    gridnew

--- apps/portalbase/portal_start.py
+++ apps/portalbase/portal_start.py
@@ -6,19 +6,19 @@ j.application.start("appserver6_test")
 
 j.logger.disable()
 
-# q.qshellconfig.checkCreateConfigFile("blobstor")
-# if "grid" not in q.qshellconfig.blobstor.getSections():
+# q.jshellconfig.checkCreateConfigFile("blobstor")
+# if "grid" not in q.jshellconfig.blobstor.getSections():
     # not configured yet
     # if not local:
-        # q.qshellconfig.blobstor.setParam("grid","ip","psc.greenitglobe.com")
-        # q.qshellconfig.blobstor.setParam("grid","secret","inc001")
-        # q.qshellconfig.blobstor.setParam("grid","type","server")
-        # q.qshellconfig.blobstor.setParam("grid","restport","9000")
-        # q.qshellconfig.blobstor.setParam("grid","namespace","grid")
+        # q.jshellconfig.blobstor.setParam("grid","ip","psc.greenitglobe.com")
+        # q.jshellconfig.blobstor.setParam("grid","secret","inc001")
+        # q.jshellconfig.blobstor.setParam("grid","type","server")
+        # q.jshellconfig.blobstor.setParam("grid","restport","9000")
+        # q.jshellconfig.blobstor.setParam("grid","namespace","grid")
     # else:
-        # q.qshellconfig.blobstor.setParam("grid","type","local")
-        # q.qshellconfig.blobstor.setParam("grid","localpath","c:\\temp\\blobstor")
-        # q.qshellconfig.blobstor.setParam("grid","namespace","grid")
+        # q.jshellconfig.blobstor.setParam("grid","type","local")
+        # q.jshellconfig.blobstor.setParam("grid","localpath","c:\\temp\\blobstor")
+        # q.jshellconfig.blobstor.setParam("grid","namespace","grid")
 
 j.manage.portal.startprocess()
 
