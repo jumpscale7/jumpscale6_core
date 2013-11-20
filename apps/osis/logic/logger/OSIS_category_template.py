30c4f8e1cc818d68f78c74a5047986c8863ff81c
Sherif Zain <zains@codescalers.com>
2013-11-20 09:17:30 +0200

    Added missing file

---
+++ apps/osis/logic/logger/OSIS_category_template.py
@@ -0,0 +1,9 @@
+from JumpScale import j
+
+parentclass=j.core.osis.getOsisImplementationParentClass("_coreobjects")  #is the name of the namespace
+
+class mainclass(parentclass):
+    """
+    """
+
+
