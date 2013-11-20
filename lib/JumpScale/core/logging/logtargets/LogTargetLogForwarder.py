70eeb2124efbd9f9594da30e6e76ad87976de47e
Jo De Boeck <deboeck.jo@gmail.com>
2013-11-20 15:04:59 +0200

    Make logtarget more robus

--- lib/JumpScale/core/logging/logtargets/LogTargetLogForwarder.py
+++ lib/JumpScale/core/logging/logtargets/LogTargetLogForwarder.py
@@ -2,16 +2,20 @@
 from JumpScale import j
 import time
 
+
+TIMEOUT = 5
+
 class LogTargetLogForwarder():
     """Forwards incoming logRecords to localclientdaemon"""
     def __init__(self, serverip=None):
+        self._lastcheck = 0
         self.connected = False
-        self.enabled = False
+        self.enabled = True
         if not serverip:
             if j.application.config.exists('grid.master.ip'):
                 serverip = j.application.config.get("grid.master.ip")
             if not serverip:
-                self.enable = False
+                self.enabled = False
                 return
         self.serverip = serverip
         self.checkTarget()
@@ -21,17 +25,18 @@ class LogTargetLogForwarder():
         check status of target, if ok return True
         for std out always True
         """
-
-        if j.system.net.tcpPortConnectionTest(self.serverip,4443)==False:
-            print "will be waiting for 10 sec if I an reach local logger."
-        if j.system.net.waitConnectionTest(self.serverip,4443,10)==False:
-            raise RuntimeError("Could not reach local logserver")
+        if self._lastcheck + TIMEOUT > time.time():
+            return self.connected
+        self.connected = j.system.net.tcpPortConnectionTest(self.serverip,4443)
+        self._lastcheck = time.time()
+        if not self.connected:
+            print "will be waiting for 5 sec if I an reach local logger."
+            return self.connected
 
         import JumpScale.grid
         self.loggerClient=j.core.grid.getZLoggerClient(ipaddr=self.serverip)
-        self.enabled=True
         j.logger.clientdaemontarget=self
-        return True
+        return self.connected
 
     def __str__(self):
         """ string representation of a LogTargetServer to ES"""
@@ -44,11 +49,13 @@ class LogTargetLogForwarder():
         """
         forward the already encoded message to the target destination
         """
-        
-        if self.enabled:            
-            # print "LOG:%s"%log
-            self.loggerClient.log(log)
-        # else:
-        #     if j.logger.logTargets==[]:  #otherwise all logmessages in this phase would go unnoticed
-        #         print log  
+        if self.enabled:
+            if not self.checkTarget():
+                return
+
+            try:
+                self.loggerClient.log(log)
+            except:
+                print 'Failed to log in %s' % self
+                self.connected = False
 
