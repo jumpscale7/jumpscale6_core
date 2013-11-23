b2903efffb94e5c3a5613988e334e831de2826dc
Kristof De Spiegeleer <kristof@incubaid.com>
2013-11-23 16:48:11 +0200

    cleanup in setup

--- setup.py
+++ setup.py
@@ -22,15 +22,17 @@ version = get_version('lib/JumpScale')
 def clean():
     print "CLEAN"
     for r,d,f in os.walk("/usr"):
-        for path in f:
-	    match=False
-	    if path.startswith("jscode") or path.startswith("jpackage") or path.startswith("jspackage") or path.startswith("jsdevelop") or path.startswith("jsreinstall"):
-	        match=True
-	    if path in ["js" or "jsshell"]:
-	        match=True	    
-	    if match:
-	        print "remove:%s" % os.path.join(r,path)
-	        os.remove(os.path.join(r,path))
+      for path in f:
+          match=False
+          if path.startswith("jscode") or path.startswith("jpackage") or path.startswith("jspackage") or path.startswith("jsdevelop")\
+              or path.startswith("jsreinstall") or path.startswith("jsprocess") or path.startswith("jslog") or path.startswith("jsshell"):
+              match=True
+          if path in ["js"]:
+              match=True      
+          if match:
+              print "remove:%s" % os.path.join(r,path)
+              os.remove(os.path.join(r,path))
+
 
 clean()
 
