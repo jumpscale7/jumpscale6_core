370f2ddf21b0e65412f654338f466335d451e5e3
Reem Khamis <khamisr@codescalers.com>
2013-12-08 18:08:57 +0200

    Tweaking of ECOs Macro

--- apps/portalbase/system/system__contentmanager/macros/page/ecos/3_main.py
+++ apps/portalbase/system/system__contentmanager/macros/page/ecos/3_main.py
@@ -18,37 +18,69 @@ def main(j, args, params, tags, tasklet):
     for result in results:
         data.append(result['count'])
         t = datetime.datetime.fromtimestamp(result['term'])
-        header = t.strftime('%Y-%m-%d %H:%M:%S')
+        header = t.strftime('%d/%m %H:%M')
         result['header'] = header
         headers.append(header)
 
     page.addJS("/lib/jquery.facetview.js")
-    C = """
+    C = r"""
 <script type="text/javascript">
 jQuery(document).ready(function($) {
+  function pad(num) {
+    var s = "0" + num;
+    return s.substr(s.length-2);
+  };
+
   function epochToStr(val) {
     var date = new Date(val * 1000);
-    return date.toDateString() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    return pad(date.getDay()) + "/" + pad(date.getMonth()) + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 
   };
   window.filterfunction = function(event, val, filtertype){
-    event.preventDefault();
+    event.defaultPrevented = true;
     if ($('a.facetview_filterchoice[rel='+filtertype+'][href='+val+']').length > 0) {
       if ($('.facetview_filterselected[rel='+filtertype+'][href='+val+']').length == 0){
-        $('a.facetview_filterchoice[rel='+filtertype+'][href='+val+']').click();
+        $('a.facetview_filterchoice[rel='+filtertype+'][href='+val+']')[0].click();
       }
     }
   };
-  function linkifyGID(val) {
-      return "<a id='gid' href='#' onclick='filterfunction(event, "+val+", this.id)'>"+ val.toString() +"</a>"
+  function linkify(id) {
+      return function(val, inFilter) {
+          if (inFilter)
+            return val;
+          var ancor = $("<a>");
+          ancor.attr('id', id);
+          ancor.attr('href', '#');
+          ancor.attr('title', val);
+          ancor.attr('onclick', "filterfunction(event, '"+ val +"', this.id)");
+          ancor.html(val);
+          return ancor[0].outerHTML;
+      };
   };
-  function linkifyNID(val) {
-      return "<a id='nid' href='#' onclick='filterfunction(event, "+val+", this.id)'>"+ val.toString() +"</a>"
+
+  function columnFormatter(size) {
+      return function(val, inFilter) {
+        if (inFilter) {
+            return val;
+        }
+        var container = $('<div>');
+        container.html(val);
+        container.css('max-width', size)
+                 .css('white-space', 'nowrap')
+                 .css('overflow', 'hidden')
+                 .css('text-overflow', 'ellipsis');
+
+        return container[0].outerHTML;
+      }
+  }
+
+  function formatJID(val, inFilter) {
+      if (!val) {
+        return "";
+      }
+      val = linkify('jid')(val, inFilter);
+      return columnFormatter('40px')(val, inFilter);
   };
-  function linkifyPID(val) {
-      return "<a id='pid' href='#' onclick='filterfunction(event, "+val+", this.id)'>"+ val.toString() +"</a>"
-  };
-  
   var hostname = window.location.href.split('/')[2]
   $('.facet-view-simple').facetview({
     search_url: 'http://'+hostname+':9200/system_eco/_search?',
@@ -70,13 +102,13 @@ jQuery(document).ready(function($) {
         {'field': 'pid', 'display': 'PID'}
     ],
     result_display: [[{field: "epoch", formatter: epochToStr},
-                      {field: "appname"},
-                      {field: "category"},
+                      {field: "appname", formatter: columnFormatter('90px')},
+                      {field: "category", formatter: columnFormatter('90px')},
                       {field: "level"},
-                      {field: "errormessage"},
-                      {field: "gid", formatter: linkifyGID},
-                      {field: "nid", formatter: linkifyNID},
-                      {field: "pid", formatter: linkifyPID},
+                      {field: "errormessage", formatter: columnFormatter('300px')},
+                      {field: "gid", formatter: linkify('gid')},
+                      {field: "nid", formatter: linkify('nid')},
+                      {field: "pid", formatter: linkify('pid')},
                       ]],
     paging: {
       size: 10
@@ -84,6 +116,15 @@ jQuery(document).ready(function($) {
   });
 
 });
+
+// Put ellipsis on the 'message' column
+setInterval(function() {
+
+  $('#facetview_results tr').each(function() {
+      var elt = $(this);
+      elt.find('td').attr('title', elt.text());
+    });
+  }, 500)
 
   </script>
 
