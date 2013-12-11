8f6187c4948e91b7b37296efc6b26d46364fe0c0
Sherif Zain <zains@codescalers.com>
2013-12-11 10:47:49 +0200

    Added params to jobslist macro

--- apps/gridportal/base/GridLogs/.macros/page/ac_joblist/1_main.py
+++ apps/gridportal/base/GridLogs/.macros/page/ac_joblist/1_main.py
@@ -1,16 +1,43 @@
-import JumpScale.grid.geventws
 import datetime
 
 def main(j, args, params, tags, tasklet):
 
     page = args.page
+    qsparams = args.requestContext.params
+    path = qsparams.pop('path', None)
+    rights = qsparams.pop('rights', None)
+
     import JumpScale.grid.osis
     osiscl = j.core.osis.getClient()
     client = j.core.osis.getClientForCategory(osiscl, 'system', 'job')
     
     header = ["ID", "Roles", "Started", "Stopped",  "JSName", "JSOrg", "Timeout", "SessionID", "Status"]
-    rows = []
-    jobs = client.search("null") # TODO refine this query
+    rows = list()
+
+    if not qsparams:
+        jobs = client.search("null")
+    else:
+        query = {'query': {'bool': {'must': list()}}}
+        if 'from' in qsparams:
+            starting = j.base.time.getEpochAgo(qsparams.get('from'))
+            qsparams.pop('from', None)
+            drange = {'range': {'timeStart': {'gte': starting}}}
+            query['query']['bool']['must'].append(drange)
+        if 'to' in qsparams:
+            ending = j.base.time.getEpochAgo(qsparams.get('to'))
+            qsparams.pop('to', None)
+            if query['query']['bool']['must']:
+                query['query']['bool']['must'][0]['range']['timeStart']['lte'] = ending
+            else:
+                drange = {'range': {'timeStart': {'lte': ending}}}
+                query['query']['bool']['must'].append(drange)
+
+        for param in qsparams:
+            term = {'term': {param: qsparams[param]}}
+            query['query']['bool']['must'].append(term)
+
+        jobs = client.search(query)
+
     if 'result' in jobs: 
         for job in jobs['result']:
             job = job['_source']
