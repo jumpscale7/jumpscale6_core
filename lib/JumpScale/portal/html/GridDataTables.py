51b2b4740dd84b1c1b46a71f9832c780563e8ebe
Reem Khamis <khamisr@codescalers.com>
2013-12-12 19:28:02 +0200

    Generate random name for DataTable so a page can have more than one

--- lib/JumpScale/portal/html/GridDataTables.py
+++ lib/JumpScale/portal/html/GridDataTables.py
@@ -23,6 +23,8 @@ class GridDataTables:
         return self.page
 
     def addTableFromURL(self, url, fieldnames):
+        import random
+        tableid = 'table%s' % random.randint(0, 1000)
 
         self.page.addCSS("%s/datatables/DT_bootstrap.css" % self.liblocation)
         # self.page.addJS("%s/datatables/DT_bootstrap.js"% self.liblocation)
@@ -32,7 +34,7 @@ class GridDataTables:
 
         C = """
 $(document).ready(function() {
-    $('#example').dataTable( {
+    $('#$tableid').dataTable( {
         "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
         "bProcessing": false,
         "bServerSide": false,
@@ -43,6 +45,7 @@ $(document).ready(function() {
     } );
 } );"""
         C = C.replace("$url", url)
+        C = C.replace("$tableid", tableid)
         self.page.addJS(jsContent=C, header=False)
 
 #<table cellpadding="0" cellspacing="0" border="0" class="display" id="example">
@@ -50,7 +53,7 @@ $(document).ready(function() {
 
         C = """
 <div id="dynamic">
-<table class="table table-striped table-bordered" id="example" border="0" cellpadding="0" cellspacing="0" width="100%">
+<table class="table table-striped table-bordered" id=$tableid border="0" cellpadding="0" cellspacing="0" width="100%">
     <thead>
         <tr>
 $fields
@@ -68,6 +71,7 @@ $fields
         for name in fieldnames:
             fieldstext += "<th>%s</th>\n" % (name)
         C = C.replace("$fields", fieldstext)
+        C = C.replace("$tableid", tableid)
 
         self.page.addMessage(C, isElement=True, newline=True)
         return self.page
