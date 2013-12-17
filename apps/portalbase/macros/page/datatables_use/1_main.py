009375725104ab4ce0970b8f6dfa0d7dec6c371a
Mohammad Tayseer <m.tayseer82@gmail.com>
2013-12-17 14:16:22 +0200

    Added flag disabled_filters to macro datatables_use

--- apps/portalbase/macros/page/datatables_use/1_main.py
+++ apps/portalbase/macros/page/datatables_use/1_main.py
@@ -2,28 +2,30 @@
 def main(j, args, params, tags, tasklet):
     params.result = page = args.page
     tags = args.tags
+    disable_filters = tags.tagExists('disable_filters') and tags.tagGet('disable_filters').lower() == 'true'
 
     modifier = j.html.getPageModifierGridDataTables(args.page)
     modifier.prepare4DataTables()
-    page.addJS(jsContent='''
-    	$(function() {
-	    	$('.dataTable').each(function() {
-	    		var table = $(this);
-	    		var numOfColumns = table.find('th').length;
-	    		var tfoot = $('<tfoot />');
-	    		for (var i = 0; i < numOfColumns; i++) {
-	    			var td = $('<td />');
-	    			td.append(
-	    				$('<input />', {type: 'text', 'class': 'datatables_filter'}).keyup(function() {
-	    					table.dataTable().fnFilter(this.value, tfoot.find('input').index(this));
-	    				})
-	    			);
-	    			tfoot.append(td);
-	    		}
-	    		table.append(tfoot);
-	    	});
-	    });
-    ''', header=False)
+    if not disable_filters:
+	    page.addJS(jsContent='''
+	    	$(function() {
+		    	$('.dataTable').each(function() {
+		    		var table = $(this);
+		    		var numOfColumns = table.find('th').length;
+		    		var tfoot = $('<tfoot />');
+		    		for (var i = 0; i < numOfColumns; i++) {
+		    			var td = $('<td />');
+		    			td.append(
+		    				$('<input />', {type: 'text', 'class': 'datatables_filter'}).keyup(function() {
+		    					table.dataTable().fnFilter(this.value, tfoot.find('input').index(this));
+		    				})
+		    			);
+		    			tfoot.append(td);
+		    		}
+		    		table.append(tfoot);
+		    	});
+		    });'''
+	    , header=False)
 
     return params
 
