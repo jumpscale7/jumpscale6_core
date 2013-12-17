6bfa365af5608fe41af7ba1a2c034c3c91f64d30
Rob Van Mieghem <rob@vanmieghemcloud.com>
2013-12-17 18:28:38 +0100

    realtime statistic directive

--- apps/gridportal/base/Grid/.macros/page/realtimestat/1_main.py
+++ apps/gridportal/base/Grid/.macros/page/realtimestat/1_main.py
@@ -3,14 +3,35 @@ def main(j, args, params, tags, tasklet):
     page = args.page
   
     qsparams = args.requestContext.params
-    nodeId = qsparams.pop('nodeId', None)
+    nodeId = qsparams.pop('nid', None)
+    statistic = qsparams.pop('statistic', None)
+
+    page.addBodyAttribute('ng-app="jumpscale"')
+
+    page.addJS(".files/lib/jquery/jquery-1.10.1.js")
+
+    page.addCSS(".files/lib/jqplot-1.0.8/jquery.jqplot.min.css")
+
+    page.addJS(".files/lib/jqplot-1.0.8/jquery.jqplot.min.js")
+    page.addJS(".files/lib/jqplot-1.0.8/plugins/jqplot.cursor.min.js")
+    page.addJS(".files/lib/jqplot-1.0.8/plugins/jqplot.canvasTextRenderer.min.js")
+    page.addJS(".files/lib/jqplot-1.0.8/plugins/jqplot.canvasAxisTickRenderer.js")
+
+    page.addJS(".files/lib/jqplot-1.0.8/plugins/jqplot.canvasAxisLabelRenderer.min.js")
+    page.addJS(".files/lib/jqplot-1.0.8/plugins/jqplot.dateAxisRenderer.js")
+
+
+
+    page.addJS(".files/lib/angular-1.2.5/angular.js")
+    page.addJS(".files/app.js")
+    page.addJS(".files/directives.js")
 
     if nodeId:
         print nodeId
 
     url = '/restmachine/system/gridmanager/getNodeSystemStats?nodeId=%s' % nodeId
 
-    page.addHTML('<div id="statisticsChart" data-chart ng-model="statisticsData" ng-url="%s" style="width: 100%;"></div>' % url)
+    page.addHTML('<div id="statisticsChart" data-chart ng-model="statisticsData" ng-url="%s" ng-stat="%s" style="width: 100%%;"></div>' % (url,statistic))
 
     params.result = page
     return params
