22cdf135584d25748b324fa1cabaf9faeb6a99ea
Mohammad Tayseer <m.tayseer82@gmail.com>
2013-12-17 13:03:33 +0200

    Added filters for individual pages

--- apps/portalbase/macros/page/datatables_use/1_main.py
+++ apps/portalbase/macros/page/datatables_use/1_main.py
@@ -4,7 +4,26 @@ def main(j, args, params, tags, tasklet):
     tags = args.tags
 
     modifier = j.html.getPageModifierGridDataTables(args.page)
-    params.result = modifier.prepare4DataTables()
+    modifier.prepare4DataTables()
+    page.addJS(jsContent='''
+    	$(function() {
+	    	$('.dataTable').each(function() {
+	    		var table = $(this);
+	    		var numOfColumns = table.find('th').length;
+	    		var tfoot = $('<tfoot />');
+	    		for (var i = 0; i < numOfColumns; i++) {
+	    			var td = $('<td />');
+	    			td.append(
+	    				$('<input />', {type: 'text', 'class': 'datatables_filter'}).keyup(function() {
+	    					table.dataTable().fnFilter(this.value, tfoot.find('input').index(this));
+	    				})
+	    			);
+	    			tfoot.append(td);
+	    		}
+	    		table.append(tfoot);
+	    	});
+	    });
+    ''', header=False)
 
     return params
 
