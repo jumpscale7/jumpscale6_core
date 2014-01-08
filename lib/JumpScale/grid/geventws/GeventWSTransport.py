eff84bd11ff63bfc130a1e74d64d2d78989cfc7e
Jo De Boeck <deboeck.jo@gmail.com>
2014-01-08 10:26:51 +0200

    Fix for blocking calles when inside gevent
    Reviewer: Hendrik Demolder

--- lib/JumpScale/grid/geventws/GeventWSTransport.py
+++ lib/JumpScale/grid/geventws/GeventWSTransport.py
@@ -4,7 +4,7 @@ import JumpScale.grid.serverbase
 from JumpScale.grid.serverbase.DaemonClient import Transport
 
 
-import requests
+import grequests as requests
 
 class GeventWSTransport(Transport):
     def __init__(self, addr="localhost", port=9999):
@@ -63,15 +63,16 @@ class GeventWSTransport(Transport):
             r = requests.post(self.url, data=data2, headers=headers,timeout=timeout)
 
         # print data
+        rcv = r.send()
 
-        if r==None:
+        if rcv==None:
             eco=j.errorconditionhandler.getErrorConditionObject(msg='timeout on request to %s'%self.url, msgpub='', \
                 category='tornado.transport')
             return "4","m",j.db.serializers.msgpack.dumps(eco.__dict__)
                     
-        if r.ok==False:
+        if rcv.ok==False:
             eco=j.errorconditionhandler.getErrorConditionObject(msg='error 500 from webserver on %s'%self.url, msgpub='', \
                 category='tornado.transport')
             return "6","m",j.db.serializers.msgpack.dumps(eco.__dict__)
 
-        return j.servers.base._unserializeBinReturn(r.content)
+        return j.servers.base._unserializeBinReturn(rcv.content)
