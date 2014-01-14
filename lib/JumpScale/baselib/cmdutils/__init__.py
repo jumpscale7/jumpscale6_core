7b336a9aa1a5170b6e43f1a91fee1d617c95d3e7
Kristof De Spiegeleer <kristof@incubaid.com>
2014-01-14 11:47:29 +0100

    when chosing packages in debug mode, make sure only the ones not in debug yet are shown

--- lib/JumpScale/baselib/cmdutils/__init__.py
+++ lib/JumpScale/baselib/cmdutils/__init__.py
@@ -56,7 +56,7 @@ def getJPackage(args, installed=None,domain=None,debug=None):
     else:
         packages = j.packages.find(name=args.name, domain=domain, version=args.version,installed=installed)
 
-    if debug:
+    if debug==False:
         debugpackages=j.packages.getDebugPackages()
         packages=[item for item in packages if item not in debugpackages]        
 
