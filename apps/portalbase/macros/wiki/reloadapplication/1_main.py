56bc3bcdd60fb7b7a70a545e5c68a130094934ef
Mohammad Tayseer <m.tayseer82@gmail.com>
2014-01-16 13:56:22 +0200

    Make ReloadAll work

--- apps/portalbase/macros/wiki/reloadapplication/1_main.py
+++ apps/portalbase/macros/wiki/reloadapplication/1_main.py
@@ -2,8 +2,7 @@ def main(j, args, params, tags, tasklet):
     params.merge(args)
 
     # temporary hack to get the application name
-    name = j.system.fs.getParentDirName(j.system.fs.getParent(j.core.portal.runningPortal.webserver.cfgdir))
-    j.core.portal.runningPortal.restartInProcess(name)
+    j.core.portal.runningPortal.restartInProcess('')
 
     params.result = ("", params.doc)
 
