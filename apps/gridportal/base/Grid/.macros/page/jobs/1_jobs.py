375ebe06683873ab1055fe573a964b58ffd6cd7b
Jo De Boeck <deboeck.jo@gmail.com>
2014-01-21 17:24:00 +0200

    Change node and jobs to be paging grids

---
+++ apps/gridportal/base/Grid/.macros/page/jobs/1_jobs.py
@@ -0,0 +1,36 @@
+
+def main(j, args, params, tags, tasklet):
+
+    page = args.page
+    nid = args.getTag("nid")
+    if not nid and args.tags.tagExists('nid'):
+        page.addMessage('Missing node id param "id"')
+        params.result = page
+        return params
+
+    jsname = args.getTag('jsname')
+    jsorganization = args.getTag('jsorganization')
+
+    filters = dict()
+    filters['nid'] = nid
+    if jsname:
+        filters['jsname'] = jsname
+    if jsorganization:
+        filters['jsorganization'] = jsorganization
+
+    modifier = j.html.getPageModifierGridDataTables(page)
+
+    def makeLink(row, field):
+        return '[%s|/grid/job?id=%s]' % (row['id'], row['id'])
+
+    fieldnames = ['Id', 'Category', 'Result', 'JSName', 'JSOrganization', 'State', 'Description']
+    fieldvalues = [makeLink, 'category', 'result', 'jsname', 'jsorganization', 'state', 'description']
+    fieldids = ['id', 'category', 'result', 'jsname', 'jsorganization', 'state', 'description']
+    modifier.addTableForModel('system', 'job', fieldids, fieldnames, fieldvalues, filters)
+
+    params.result = page
+    return params
+
+
+def match(j, args, params, tags, tasklet):
+    return True
