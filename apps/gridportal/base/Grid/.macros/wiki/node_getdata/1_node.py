c52e0cc34ab3ff00dba81ecc747c26b280d82265
Jo De Boeck <deboeck.jo@gmail.com>
2014-01-23 14:19:41 +0200

    When wrong id is giving give proper error

--- apps/gridportal/base/Grid/.macros/wiki/node_getdata/1_node.py
+++ apps/gridportal/base/Grid/.macros/wiki/node_getdata/1_node.py
@@ -3,17 +3,15 @@ import json
 def main(j, args, params, tags, tasklet):
 
     #macro puts obj info as params on doc, when show used as label, shows the content of the obj in nicely structured code block
+    nid = args.getTag('id')
+    node = j.apps.system.gridmanager.getNodes(id=nid)
+    if not node:
+        params.result = ('Node with id %s not found' % nid, args.doc)
+        return params
 
     def objFetchManipulate(id):
-        actor = j.apps.actorsloader.getActor("system", "gridmanager")
         #retrieve nods from actor method
-
-        node = actor.getNodes(id=id)
-        if not node:
-            params.result = ('Node with id %s not found' % id, doc)
-            return params
-
-        obj=actor.getNodes(id=id)[0] #returns 1 node in array (is how the getNodes method works)
+        obj=node[0] #returns 1 node in array (is how the getNodes method works)
         #obj is a dict
 
         obj["ipaddr"]=", ".join(obj["ipaddr"])
@@ -27,7 +25,7 @@ def main(j, args, params, tags, tasklet):
         obj["netaddr"]=r
         return obj
 
-    push2doc=j.apps.actorsloader.getActor("system", "contentmanager").extensions.macrohelper.push2doc
+    push2doc=j.apps.system.contentmanager.extensions.macrohelper.push2doc
 
     return push2doc(args,params,objFetchManipulate)
 
