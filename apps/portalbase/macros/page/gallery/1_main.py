1dcef633d282e6ca11fbc808456ce95d080c2090
Mohammad Tayseer <m.tayseer82@gmail.com>
2014-01-30 16:12:57 +0200

    Made the gallery full screen

--- apps/portalbase/macros/page/gallery/1_main.py
+++ apps/portalbase/macros/page/gallery/1_main.py
@@ -69,17 +69,12 @@ def main(j, args, params, tags, tasklet):
     C += '</div></div>'
 
     C += """
-    <div id="modal-gallery" class="modal modal-gallery hide fade modal-loading " tabindex="-1"  aria-hidden="true">
+    <div id="modal-gallery" class="modal modal-gallery hide fade modal-loading modal-fullscreen" tabindex="-1"  aria-hidden="true">
     <div class="modal-header">
         <a class="close" data-dismiss="modal">&times;</a>
         <h3 class="modal-title"></h3>
     </div>
     <div class="modal-body"><div class="modal-image"></div></div>
-    <div class="modal-footer">
-        <a class="btn btn-primary modal-next">Next <i class="icon-arrow-right icon-white"></i></a>
-        <a class="btn btn-info modal-prev"><i class="icon-arrow-left icon-white"></i> Previous</a>
-        <a class="btn btn-success modal-play modal-slideshow" data-slideshow="5000"><i class="icon-play icon-white"></i> Slideshow</a>
-    </div>
     </div>
     """
     page.addMessage(C)
