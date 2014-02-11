1f5f8fc3a10730600b0b4a49dbdb4c884ff1ff98
Kristof De Spiegeleer <kristof@incubaid.com>
2014-02-11 17:44:30 +0100

    blobserver2

--- apps/blobserver2/client.py
+++ apps/blobserver2/client.py
@@ -12,28 +12,39 @@ login="root"
 client= j.servers.zdaemon.getZDaemonClient("127.0.0.1",port=2345,user=login,passwd=passwd,ssl=False,sendformat='m', returnformat='m',category="blobserver")
 
 blob=""
-for i in range(1024):#*1024*4):
+for i in range(1024*1024*4):
     blob+="A"
 #4MB
 
 hash=j.tools.hash.md5_string(blob)
+namespace="test"
+
+client.deleteNamespace("test")
 
 print "start"
-client.set(hash,blob,repoId="repo1") 
-client.set(hash,blob,repoId="repo2")
-blob2=client.get(hash)
 
-md=client.getMD(hash)
-print md
+client.set(hash,namespace,blob,repoId="repo1") 
+client.set(hash,namespace,blob,repoId="repo2")
 
-blob2=client.delete(hash)
-
-md=client.getMD(hash)
-print md
+blob2=client.get(hash,namespace)
 
 assert blob2==blob
 
-assert client.exists(hash)==True 
+md=client.getMD(hash,namespace)
+print md
+
+blob2=client.delete(hash,namespace,repoId="repo1")
+
+md=client.getMD(hash,namespace)
+print md
+
+assert client.exists(hash,namespace,repoId="repo2")==True
+assert client.exists(hash,namespace,repoId="repo1")==False
+assert client.exists(hash,namespace)==True
+
+blob2=client.delete(hash,namespace,repoId="repo2")
+assert client.exists(hash,namespace)==False
+
 
 j.application.stop()
 
