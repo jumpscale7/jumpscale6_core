1b98d7638aca2d30f7e78038fd4b133556f7f8ec
Mohammad Tayseer <m.tayseer82@gmail.com>
2014-02-12 15:01:55 +0200

    Updated plantuml macro to use jar instead of using online PlantUML website

--- apps/portalbase/macros/page/plantuml/1_plantuml.py
+++ apps/portalbase/macros/page/plantuml/1_plantuml.py
@@ -1,16 +1,15 @@
-import random
+from JumpScale.portal.macrolib.plantuml import run_plant_uml, GraphVizNotInstalled
+
 def main(j, args, params, tags, tasklet):
     page = args.page
-    page.addJS(jsLink='/lib/plantuml/rawdeflate.js')
-    page.addJS(jsLink='/lib/plantuml/plantuml.js')
 
-    random_id = random.randint(1, 9999999)
-    page.addMessage('<textarea id="uml_{}" style="display: none">{}</textarea>'.format(random_id, args.cmdstr))
-    page.addMessage('<img id="uml_im_{}">'.format(random_id))
-    page.addJS(header=False, jsContent='''
-               compress(document.getElementById('uml_{0}').value, "uml_im_{0}")
-               '''.format(random_id))
-
+    space_name = args.doc.getSpaceName()
+    try:
+        img_name = run_plant_uml(space_name, args.cmdstr, output_path='.files/img/plantuml')
+    except GraphVizNotInstalled:
+        page.addMessage('<i class="alert alert-error">GraphViz is not installed</i>')
+    else:
+        page.addMessage('<img src="/$$space/.files/img/plantuml/{}">'.format(img_name))
 
     params.result = page
     return params
