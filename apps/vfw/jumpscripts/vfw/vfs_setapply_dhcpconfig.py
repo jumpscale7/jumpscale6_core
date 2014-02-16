c3467ca20e903764d32bf0cb401d5f913367669a
Sherif Zain <zains@codescalers.com>
2014-02-16 13:40:08 +0200

    Fixed vfs_setapply_dhcpconfig jumpscript

--- apps/vfw/jumpscripts/vfw/vfs_setapply_dhcpconfig.py
+++ apps/vfw/jumpscripts/vfw/vfs_setapply_dhcpconfig.py
@@ -1,4 +1,6 @@
 from JumpScale import j
+import JumpScale.lib.dhcp
+import JumpScale.lib.lxc
 
 descr = """
 configure DHCP
@@ -14,15 +16,11 @@ roles = ["*"]
 
 
 def action(name, fromIP, toIP, interface):
-    import JumpScale.lib.dhcp
-    import JumpScale.lib.lxc
-
-    host = j.system.platform.lxc.getip(name)
+    host = j.system.platform.lxc.getIp(name)
     password = j.application.config.get('system.superadmin.passwd')
 
     cl = j.system.platform.dhcp.get(host, password)
     cl.configure(fromIP, toIP, interface)
-    return "done"
 
 
 
