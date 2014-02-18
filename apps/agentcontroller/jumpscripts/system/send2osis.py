fcc6ff5931622b3c0070313890224b94a55a7e74
Jo De Boeck <deboeck.jo@gmail.com>
2014-02-18 16:58:11 +0200

    Sent stats to osis and make osis forward stats to graphite

--- apps/agentcontroller/jumpscripts/system/send2osis.py
+++ apps/agentcontroller/jumpscripts/system/send2osis.py
@@ -10,11 +10,17 @@ license = "bsd"
 version = "1.0"
 category = "monitoring.send2carbon"
 period = 10#120 #always in sec
-enable=False
+enable=True
 async=False
 roles = ["osis.db.graphite"]
 
 def action():
-    print "SEND to CARBON"
-    j.system.stataggregator.send2carbon()
+    stats = list()
+    for key, stat in j.system.stataggregator.stats.iteritems():
+        if stat.memonly:
+            continue
+        avg, mag = stat.getAvgMax()
+        stats.append({'key': key, 'value': avg})
+    if stats:
+        j.processmanager.cache.statobject.osis.set(stats)
 
