86c45a85c764946185d47a5d51f3ed401e32b67b
Kristof De Spiegeleer <kristof@incubaid.com>
2014-03-02 08:03:14 +0100

    tests for workers and specs

---
+++ apps/agentcontroller/processmanager/processmanagercmds/WorkerCmds.py
@@ -0,0 +1,36 @@
+from JumpScale import j
+import time
+
+class WorkerCmds():
+
+    def __init__(self,daemon=None):
+        self._name="worker"
+        self.daemon=daemon
+        self._adminAuth=daemon._adminAuth
+
+    def getQueuedJobs(self,format="json",session=None):        
+        """
+        @format can be json or wiki
+        """
+        if session<>None:
+            self._adminAuth(session.user,session.passwd)  
+        # use complement RedisWorkerFactory class see getQueuedJobs
+        #only use local redis on port 6678
+        
+    def getFailedJobs(self,format="json",session=None):
+        """
+        @format can be json or wiki
+        """
+        if session<>None:
+            self._adminAuth(session.user,session.passwd)  
+        # use complement RedisWorkerFactory class make new method
+        #only use local redis on port 6678
+
+    def getJob(self,session=None):        
+        """
+        """
+        if session<>None:
+            self._adminAuth(session.user,session.passwd)  
+        # use complement RedisWorkerFactory class make new method
+        #only use local redis on port 6678
+
