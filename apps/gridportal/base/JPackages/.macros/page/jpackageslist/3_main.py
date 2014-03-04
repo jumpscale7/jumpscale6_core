983498a732f3a706abb3fb74de55b088a400ab6f
Jo De Boeck <deboeck.jo@gmail.com>
2014-03-04 12:58:08 +0200

    Change gridportal packagemanager to use jumpscripts and ditch jpackagecmds

--- apps/gridportal/base/JPackages/.macros/page/jpackageslist/3_main.py
+++ apps/gridportal/base/JPackages/.macros/page/jpackageslist/3_main.py
@@ -2,24 +2,24 @@ def main(j, args, params, tags, tasklet):
 
     page = args.page
     attributes = "class='nav nav-list' style='-moz-column-count: 3; -webkit-column-count:3; column-count:3;'"
-    def _getDomainPackages(d):
-        page.addHeading(d.domainname, 2)
-        packages = sorted(d.getJPackages(), key=lambda x: x.name)
-        for p in packages:
-            href = '/jpackages/JPackageShow?domain=%s&name=%s&version=%s' % (d.domainname, p.name, p.version)
-            icon = 'icon-ok' if p.isInstalled() else 'icon-remove'
-            page.addBullet("<a href='%s'><i class='%s'></i> %s</a>" % (href, icon, p.name), attributes=attributes)
 
     domain = args.tags.tagGet('domain', '')
-    if not domain:
-        #return all packages
-        for d in j.packages.domains:
-            _getDomainPackages(d)
+    nid = args.tags.tagGet('nid', j.application.whoAmI.nid)
+    jpackagelist = j.apps.system.packagemanager.getJPackages(domain=domain, nid=nid)
+    jpackages = dict()
+    for jp in jpackagelist:
+        if jp['domain'] not in jpackages:
+            jpackages[jp['domain']] = list()
+        jpackages[jp['domain']].append(jp)
+  
+    for domain in sorted(jpackages.keys()):
+        page.addHeading(domain, 2)
+        for jp in sorted(jpackages[domain], key=lambda x: x['name'].lower()):
+            href = '/jpackages/JPackageShow?domain=%s&name=%s&version=%s' % (domain, jp['name'], jp['version'])
+            icon = 'icon-ok' if jp['installed'] else 'icon-remove'
+            page.addBullet("<a href='%s'><i class='%s'></i> %s</a>" % (href, icon, jp['name']), attributes=attributes)
 
-    else:
-        #return packages of domain
-        d = j.packages.getDomainObject(domain)
-        _getDomainPackages(d)
+        
 
     params.result = page
     return params
