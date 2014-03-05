26005386bfa7041205593d0e8a8f2925169e08f7
Sherif Zain <zains@codescalers.com>
2014-03-05 16:37:03 +0200

    Added: acjobs and workersjobs macros

---
+++ apps/gridportal/base/Grid/.macros/wiki/acjobs/3_acjobs.py
@@ -0,0 +1,31 @@
+def main(j, args, params, tags, tasklet):
+
+    import JumpScale.grid.agentcontroller
+
+    def _getJobLine(job):
+        start=j.base.time.epoch2HRDateTime(job['timeStart'])
+        if job['timeStop']==0:
+            stop=""
+        else:
+            stop=j.base.time.epoch2HRDateTime(job['timeStop'])
+        line="|%s|%s|%s|%s|%s|%s|%s|%s|"%(job['id'],job['jscriptid'],job['category'],job['cmd'],start,stop,job['state'],job['queue'])
+        return line
+
+    doc = args.doc
+    out = list()
+    out.append("h2. AgentController Jobs")
+    out.append("{{datatables_use}}}}\n")
+    out.append('||ID||JScriptID||Category||Command||Start time||Stop time||State||Queue||')
+    
+    acclient = j.clients.agentcontroller.get()
+    jobs = acclient.getActiveJobs()
+    for job in jobs:
+        out.append(_getJobLine(job))
+
+    out = '\n'.join(out)
+    params.result = (out, doc)
+
+    return params
+
+def match(j, args, params, tags, tasklet):
+    return True
---
+++ apps/gridportal/base/Grid/.macros/wiki/workersjobs/3_workersjobs.py
@@ -0,0 +1,21 @@
+def main(j, args, params, tags, tasklet):
+
+    import JumpScale.grid.agentcontroller
+
+    doc = args.doc
+    nid = args.getTag('nid')
+    out = list()
+    out.append("h2. Node '%s' Queued Jobs" % nid)
+    out.append("{{datatables_use}}}}\n")
+    out.append('||ID||JScriptID||Category||Command||Start time||Stop time||State||Queue')
+
+    workerscl = j.clients.agentcontroller.getClientProxy(category="worker")
+    jobs = workerscl.getQueuedJobs(format='wiki', _agentid=nid)
+    out.append(jobs)
+
+    params.result = ('\n'.join(out), doc)
+
+    return params
+
+def match(j, args, params, tags, tasklet):
+    return True
