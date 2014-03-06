4f75a44bb4c4c599342438a69105305b9483e33f
Reem Khamis <khamisr@codescalers.com>
2014-03-06 12:38:25 +0200

    Fix macroExecutor to not go into the same macro repeatedly

--- lib/JumpScale/portal/portal/MacroExecutor.py
+++ lib/JumpScale/portal/portal/MacroExecutor.py
@@ -221,22 +221,23 @@ class MacroExecutorPage(MacroExecutorBase):
 
 class MacroExecutorWiki(MacroExecutorBase):
 
-    def execMacrosOnContent(self, content, doc, paramsExtra={}, recursivedepth=0, ctx=None):
-        recursivedepth += 1
+    def execMacrosOnContent(self, content, doc, paramsExtra={}, ctx=None):
+        recursivedepth = 0
         if ctx != None:
             content = doc.applyParams(ctx.params, findfresh=True, content=content)
         if paramsExtra != {}:
             content = doc.applyParams(paramsExtra, findfresh=True, content=content)
         macrostrs = self.findMacros(doc, content)
-        for macrostr in macrostrs:
-            # print "EXEC MACRO ONCONTENT:"
-            # print macrostr
-            # print recursivedepth
+
+        while macrostrs:
+            recursivedepth += 1
             if recursivedepth > 20:
-                content += 'ERROR: recursive error in executing macro %s' % macrostr
+                content += 'ERROR: recursive error in executing macros'
                 return content, doc
-            content, doc = self.executeMacroOnContent(content, macrostr, doc, paramsExtra, ctx=ctx)
-            content, doc = self.execMacrosOnContent(content, doc, paramsExtra, recursivedepth, ctx=ctx)  # work recursive see if other macro's
+                
+            for macrostr in macrostrs:
+                content, doc = self.executeMacroOnContent(content, macrostr, doc, paramsExtra, ctx=ctx)
+            macrostrs = self.findMacros(doc, content)
         return content, doc
 
     def executeMacroOnContent(self, content, macrostr, doc, paramsExtra=None, ctx=None):
