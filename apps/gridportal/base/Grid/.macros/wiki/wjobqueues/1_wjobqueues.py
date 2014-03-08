d6e3bd4263d95471c515143bade03e50891ea000
Jo De Boeck <deboeck.jo@gmail.com>
2014-03-08 10:56:47 +0200

    If one node his processmanager fails try to get infor about other nodes

--- apps/gridportal/base/Grid/.macros/wiki/wjobqueues/1_wjobqueues.py
+++ apps/gridportal/base/Grid/.macros/wiki/wjobqueues/1_wjobqueues.py
@@ -15,6 +15,7 @@ def main(j, args, params, tags, tasklet):
 
     out = list()
     out = ['||Node ID||Default||HyperVisor||IO||']
+    addnote = False
     for node in j.apps.system.gridmanager.getNodes():
         nid = node['id']
         data = {'default': 0, 'io': 0, 'hypervisor': 0, 'nid':nid}
@@ -22,15 +23,17 @@ def main(j, args, params, tags, tasklet):
         with gevent.Timeout(3, False):
             jobs = wclient.getQueuedJobs(queue=None, _agentid=nid)
         if jobs is None:
-            errmsg = 'Could not reach ProcessManager. Please check your services.'
-            params.result = (errmsg, doc)
-            return params
+            addnote = True
+            out.append('|[%(nid)s|workersjobs?nid=%(nid)s]|N/A*|N/A*|N/A*|' % (data))
+            continue
 
         for job in jobs:
             if job['queue'] in data:
                 data[job['queue']] += 1
         out.append('|[%(nid)s|workersjobs?nid=%(nid)s]|%(default)s|%(hypervisor)s|%(io)s|' % (data))
 
+    if addnote:
+        out.append("&#42; Means data could not be retreived from ProcessManager of that node, likely it is not running.")
     out = '\n'.join(out)
 
     params.result = (out, doc)
