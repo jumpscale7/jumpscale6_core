16803da16173564bd82648bf995ebddf92c7f91b
Reem Khamis <khamisr@codescalers.com>
2014-03-25 11:14:52 +0200

    Workers watchdog jumpscript

---
+++ apps/agentcontroller/jumpscripts/monitoring/workers_watchdog.py
@@ -0,0 +1,28 @@
+from JumpScale import j
+import JumpScale.baselib.redis
+
+descr = """
+Monitor CPU and mem of worker
+"""
+
+organization = "jumpscale"
+name = 'workerswatchdog'
+author = "khamisr@codescalers.com"
+license = "bsd"
+version = "1.0"
+category = "monitor.workerswatchdog"
+
+period = 900 #always in sec
+enable = True
+async = False
+roles = ["*"]
+
+
+def action():
+    rediscl = j.clients.redis.getGeventRedisClient('127.0.0.1', 7768)
+    workers = {'worker_default_0': '-2m', 'worker_hypervisor_0': '-10m', 'worker_io_0': '-2h', 'worker_default_1': '-2m'}
+    for worker, timeout in workers.iteritems():
+        lastactive = int(rediscl.hget('workers:watchdog', worker))
+        if j.base.time.getEpochAgo(timeout) > lastactive:
+            j.errorconditionhandler.raiseOperationalWarning('Worker %s seems to have timed out' % worker, 'monitoring')
+    
