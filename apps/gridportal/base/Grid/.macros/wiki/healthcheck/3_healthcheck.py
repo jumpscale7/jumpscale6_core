95d049a12accb4257d2d0672816f5f8f72ddd242
Reem Khamis <khamisr@codescalers.com>
2014-03-27 15:05:19 +0200

    Dashboard healthchecker

--- apps/gridportal/base/Grid/.macros/wiki/healthcheck/3_healthcheck.py
+++ apps/gridportal/base/Grid/.macros/wiki/healthcheck/3_healthcheck.py
@@ -5,20 +5,19 @@ def main(j, args, params, tags, tasklet):
     doc = args.doc
     
     out = list()
-    checks, errors = j.core.grid.healthchecker.checkStatusAllNodes()
-    for nid, check in checks.iteritems():
-        check = check['healthcheck']
-        if not check['health']:
-            out.append('h4. Something on node %s is not running.' % nid)
-            out.append("""{{cssstyle
-                        h4 { color: red;}
-                    }}""")
-
-    if len(out) == 0:
+    resutls, errors = j.core.grid.healthchecker.runAll()
+    if errors:
+        nodes = errors.keys()
+        out.append('h4. Something on node(s) %s is not running.' % ', '.join([str(x) for x in nodes]))
+        out.append('For more details, check [here|/grid/checkstatus]')
+        out.append("""{{cssstyle
+                    h4 { color: red;}
+                }}""")
+    else:
         out.append("""{{cssstyle\n
                 h4 { color: green;  }\n
                 }}""")
-        out.append('h4. Healthcheck: OK')
+        out.append('h4. Everything seems to be OK')
 
     # lastchecked = checks.get('time')
     # if lastchecked:
