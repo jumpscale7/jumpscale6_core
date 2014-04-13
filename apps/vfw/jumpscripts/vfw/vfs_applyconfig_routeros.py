e3563547eed5d060aec9f84f2b5437ad726d86b5
Hendrik Demolder <hendrik.demolder@gmail.com>
2014-04-13 08:58:29 +0200

    Add routeros applyconfig implementation

---
+++ apps/vfw/jumpscripts/vfw/vfs_applyconfig_routeros.py
@@ -0,0 +1,27 @@
+from JumpScale import j
+import JumpScale.baselib.remote
+
+descr = """
+Applies the rules in the passed fwobject to the given LXC machine name
+"""
+
+name = "vfs_applyconfig_routeros"
+category = "vfw"
+organization = "jumpscale"
+author = "hendrik@incubaid.com"
+license = "bsd"
+version = "1.0"
+roles = ["vfw.host"]
+async = True 
+
+def action(name, fwobject):
+
+    host = fwobject['host']
+    username = fwobject['username']
+    password = fwobject['login']
+
+    ro = routeros.RouterOS.RouterOS(host, username, password)
+    ro.deletePortForwardRules(tags='cloudbroker')
+    for rule in fwobject['tcpForwardRules']:  
+        ro.addPortForwardRule(rule['fromAddr'], rule['fromPort'], rule['toAddr'], rule['toPort'], tags='cloudbroker')
+    return True
