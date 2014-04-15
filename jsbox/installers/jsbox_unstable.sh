5a911c07344b2a52c399967854d954615fbf8308
Jo De Boeck <deboeck.jo@gmail.com>
2014-04-15 08:26:12 +0200

    Make it possible to specify syncserver

--- jsbox/installers/jsbox_unstable.sh
+++ jsbox/installers/jsbox_unstable.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-rsync -av -v 95.85.60.252::download/unstable/jsbox/ /opt/jsbox/  --delete-after --modify-window=60 --compress --stats  --progress
-rsync -av -v 95.85.60.252::download/unstable/jsbox_data/ /opt/jsbox_data/  --delete-after --modify-window=60 --compress --stats  --progress
+RSYNCSERVER=95.85.60.252
+if [ -n $1 ]; then
+    RSYNCSERVER=$1
+fi
+
+rsync -av -v $RSYNCSERVER::download/unstable/jsbox/ /opt/jsbox/  --delete-after --modify-window=60 --compress --stats  --progress
+rsync -av -v $RSYNCSERVER::download/unstable/jsbox_data/ /opt/jsbox_data/  --delete-after --modify-window=60 --compress --stats  --progress
 sh /opt/jsbox/tools/init.sh
 export JSBASE=/opt/jsbox
 jpackage install -n redis
