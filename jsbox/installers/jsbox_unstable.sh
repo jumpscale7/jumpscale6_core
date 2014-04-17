3279fff91196091d80dd853fe3bc1592fa8851c4
Jo De Boeck <deboeck.jo@gmail.com>
2014-04-17 12:06:50 +0200

    Remove braking jsbox init.sh

--- jsbox/installers/jsbox_unstable.sh
+++ jsbox/installers/jsbox_unstable.sh
@@ -6,7 +6,6 @@ fi
 
 rsync -av -v $RSYNCSERVER::download/unstable/jsbox/ /opt/jsbox/  --delete-after --modify-window=60 --compress --stats  --progress
 rsync -av -v $RSYNCSERVER::download/unstable/jsbox_data/ /opt/jsbox_data/  --delete-after --modify-window=60 --compress --stats  --progress
-sh /opt/jsbox/tools/init.sh
 export JSBASE=/opt/jsbox
 jpackage install -n redis
 
