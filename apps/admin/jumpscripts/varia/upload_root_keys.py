14156cd4e3ac73cdcc5e14b0d897607ac749e15c
Jo De Boeck <deboeck.jo@gmail.com>
2014-05-02 21:06:25 +0200

    Update from vscalers_sysadmin

--- apps/admin/jumpscripts/varia/upload_root_keys.py
+++ apps/admin/jumpscripts/varia/upload_root_keys.py
@@ -24,22 +24,34 @@ def action(node):
     keys = []
     cuapi=node.cuapi
 
-    from IPython import embed
-    print "DEBUG NOW ooo"
-    embed()
-    
+    tags=j.core.tags.getObject(node.args.extra)
 
-    # Fetch keys from repo
     basepath=j.dirs.replaceTxtDirVars(j.application.config.get("admin.basepath"))
     d = j.system.fs.joinPaths(basepath, 'identities')
     if not j.system.fs.exists(path=d):
         raise RuntimeError("cannot find basepath:%s"%d)
-    for u in j.system.fs.listDirsInDir(d):
+
+    if tags.labelExists("system"):
+        #only use system key
+        u = j.system.fs.joinPaths(basepath, 'identities','system')
         filename = j.system.fs.joinPaths(u, 'id.hrd')
         hrd=j.core.hrd.getHRD(filename)
         pkey=hrd.get("id.key.dsa.pub")
         keys.append(pkey)
-    print "Found", len(keys), "public ssh keys"
+        print "Found", len(keys), "public system ssh keys"
+        for name in ["id_dsa","id_dsa.pub"]:
+            u = j.system.fs.joinPaths(basepath, 'identities','system',name)
+            j.system.fs.copyFile(u,"/root/.ssh/%s"%name)
+            j.system.fs.chmod("/root/.ssh/%s"%name,384)
+    else:
+        # Fetch keys from repo
+        for u in j.system.fs.listDirsInDir(d):
+
+            filename = j.system.fs.joinPaths(u, 'id.hrd')
+            hrd=j.core.hrd.getHRD(filename)
+            pkey=hrd.get("id.key.dsa.pub")
+            keys.append(pkey)
+        print "Found", len(keys), "public ssh keys"
 
     #Remove current keys
     cuapi.run("rm -f /root/.ssh/authorized_keys")
@@ -47,5 +59,6 @@ def action(node):
     #Put new keys
     for key in keys:
         print key
-        cuapi.ssh_authorize('root', key)
+        print cuapi.ssh_authorize('root', key)
+        print "key added"
 
