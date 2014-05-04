c78a62c6389731c1912dc666c24eb982672e7b0b
Jo De Boeck <deboeck.jo@gmail.com>
2014-05-04 17:47:37 +0200

    Remove alert stuff from eventhandler

--- lib/JumpScale/core/errorhandling/EventHandler.py
+++ lib/JumpScale/core/errorhandling/EventHandler.py
@@ -1,44 +1,10 @@
 from JumpScale import j
 
-try:
-    import ujson as json
-except ImportError:
-    import json
-
-
-class AlertClient(object):
-    def __init__(self, ip, port):
-        import JumpScale.baselib.webdis
-        self._ip = ip
-        self._port = port
-        self._queue = 'queues:alerts'
-        self._client = j.clients.webdis.get(ip, port)
-
-    def sendAlert(self, gid, nid, category, state, value, ecoguid=None):
-        msg = {'nid': nid,
-               'gid': gid,
-               'category': category,
-               'state': state,
-               'ecoguid': ecoguid,
-               }
-        self._client.rpush(self._queue, json.dumps(msg))
-
 class EventHandler(object):
 
     def __init__(self):
         self.__aclient = None
 
-    @property
-    def _aclient(self):
-        if self.__aclient:
-            return self.__aclient
-        else:
-            ip = j.application.config.get('alert.client.ip')
-            port = j.application.config.getInt('alert.client.port')
-            self.__aclient = AlertClient(ip, port)
-            return self.__aclient
-
-    
     def bug_critical(self,msg,category="",e=None):
         """
         will die
@@ -77,9 +43,3 @@ class EventHandler(object):
         if e<>None:
             msg+="\nERROR:%s\n"%e
         j.errorconditionhandler.raiseOperationalCritical(msg,category=category,die=False)
-
-    def raiseAlert(self, category, state, value=None, ecoguid=None, gid=None, nid=None):
-        gid = gid or j.application.whoAmI.gid
-        nid = nid or j.application.whoAmI.nid
-        self._aclient.sendAlert(gid, nid, category, state, value, ecoguid)
-
