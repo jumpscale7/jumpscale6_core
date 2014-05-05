ee06e2564f2b068a8edce6b323513a0dada44565
Kristof De Spiegeleer <kristof@incubaid.com>
2014-05-05 22:14:00 +0200

    admintools

--- lib/JumpScale/baselib/redis/geventredis/RedisQueue.py
+++ lib/JumpScale/baselib/redis/geventredis/RedisQueue.py
@@ -1,8 +1,10 @@
+# import redis
 
+# import geventredis as redis
 
-class CRedisQueue(object):
+class RedisQueue(object):
     """Simple Queue with Redis Backend"""
-    def __init__(self, redis, name, namespace):
+    def __init__(self, redis,name, namespace='queue'):
         """The default connection parameters are: host='localhost', port=7768, db=0"""
         self.__db= redis
         self.key = '%s:%s' %(namespace, name)
@@ -26,12 +28,23 @@ class CRedisQueue(object):
         if necessary until an item is available."""
         if block:
             item = self.__db.blpop(self.key, timeout=timeout)
+            if item:
+                item = item[1]
         else:
             item = self.__db.lpop(self.key)
 
-        if item:
-            item = item[1]
         return item
+
+    def fetch(self, block=True, timeout=None):
+        """ Like get but without remove"""
+        if block:
+            item = self.__db.brpoplpush(self.key, self.key, timeout)
+        else:
+            item = self.__db.lindex(self.key, 0)
+        return item
+
+    def set_expire(self, time):
+        self.__db.expire(self.key, time)
 
     def get_nowait(self):
         """Equivalent to get(False)."""
