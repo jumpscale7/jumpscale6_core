0c19af2889fe586ef69a23c1b28fed18bd3623c2
Sherif Zain <zains@codescalers.com>
2014-05-07 11:19:25 +0200

    Fixed: execute fails in parallel mode via replacing credis with redis (credis is not thread safe)

--- lib/JumpScale/baselib/admin/Admin.py
+++ lib/JumpScale/baselib/admin/Admin.py
@@ -4,7 +4,7 @@ import sys
 # import importlib
 import imp
 import ujson
-import JumpScale.baselib.credis
+import JumpScale.baselib.redis
 import copy
 import time
 
@@ -268,8 +268,8 @@ class Admin():
         self.applyconfiglocal() #set hosts
         self.js={}
         self.loadJumpscripts()
-        
-        self.redis = j.clients.credis.getRedisClient("127.0.0.1", 7768)
+        # DO NOT USE CREDIS IN THIS CONTEXT, NOT THREAD SAFE
+        self.redis = j.clients.redis.getRedisClient("127.0.0.1", 7768)
         self.nodes={}
         self.errors=[]
         self._log=""
