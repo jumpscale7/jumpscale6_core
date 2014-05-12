359c36afee7346f36d0c739ba33b26f2a6a8d8ce
Sherif Zain <zains@codescalers.com>
2014-05-12 13:29:28 +0200

    Fixed: worker status reporting correctly now

--- apps/agentcontroller/jumpscripts/monitoring_infogathering/info_gather_workerstatus.py
+++ apps/agentcontroller/jumpscripts/monitoring_infogathering/info_gather_workerstatus.py
@@ -19,21 +19,21 @@ log=False
 
 def action():
     rediscl = j.clients.redis.getGeventRedisClient('127.0.0.1', 7768)
-    timemap = {'default': '-2m', 'io': '-2h', 'hypervisor': '-10m'}
+    timemap = {'default': '-2m', 'io': '-2h', 'hypervisor': '-10m','process':'-1m'}
     result = dict()
-    prefix = 'workers__worker_'
-    workers = [ x[len(prefix):] for x in j.tools.startupmanager.listProcesses() if x.startswith(prefix) ]
-    for worker in workers:
-        timeout = timemap.get(worker.split('_')[0])
-        lastactive = int(rediscl.hget('workers:watchdog', 'worker_%s' % worker) or 0)
-        pids = j.system.process.getProcessPid(worker)
-        stats = {'cpu': 0, 'mem': 0, 'lastactive': lastactive, 'state': 'HALTED'}
-        for pid in pids:
-            processobj = j.system.process.getProcessObject(pid)
-            stats['cpu'] += processobj.get_cpu_percent()
-            stats['mem'] += processobj.get_memory_info()[0]
-        if j.base.time.getEpochAgo(timeout) < lastactive and pids:
-            stats['state'] = 'RUNNING'
-        result[worker] = stats
+    workers_processdefs = j.tools.startupmanager.getProcessDefs('workers')
+    for processdef in workers_processdefs:
+        for i in range(1, processdef.numprocesses + 1):
+            worker = '%s_%s' % (processdef.name, i)
+            lastactive = rediscl.hget('workers:watchdog', worker) or 0
+            timeout = timemap.get(processdef.name.split('_')[1])
+            pids = j.system.process.getProcessPid(worker)
+            stats = {'cpu': 0, 'mem': 0, 'lastactive': lastactive, 'state': 'HALTED'}
+            for pid in pids:
+                processobj = j.system.process.getProcessObject(pid)
+                stats['cpu'] += processobj.get_cpu_percent()
+                stats['mem'] += processobj.get_memory_info()[0]
+            if j.base.time.getEpochAgo(timeout) < lastactive and pids:
+                stats['state'] = 'RUNNING'
+            result[worker] = stats
     return result
-    
