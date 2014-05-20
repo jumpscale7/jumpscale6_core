69aa2172225ea82c8829867942a7ed783ba5b096
Reem Khamis <khamisr@codescalers.com>
2014-05-20 15:27:37 +0300

    fix in jobs macro, filtering on nid

--- apps/gridportal/base/Grid/.macros/page/jobs/1_jobs.py
+++ apps/gridportal/base/Grid/.macros/page/jobs/1_jobs.py
@@ -9,7 +9,7 @@ def main(j, args, params, tags, tasklet):
     page = args.page
     nid = args.getTag("nid")
     if not nid and args.tags.tagExists('nid'):
-        page.addMessage('Missing node id param "id"')
+        page.addMessage('Missing node id param "nid"')
         params.result = page
         return params
 
@@ -40,9 +40,9 @@ def main(j, args, params, tags, tasklet):
             pass
         return j.html.escape(str(result))
 
-    fieldnames = ['Time Start', 'Category', 'Command', 'Result', 'State']
-    fieldvalues = [makeLink, 'category', 'cmd', makeResult, 'state']
-    fieldids = ['timeStart', 'category', 'cmd', 'result', 'state']
+    fieldnames = ['Time Start', 'Category', 'Command', 'Result', 'State', 'Node ID']
+    fieldvalues = [makeLink, 'category', 'cmd', makeResult, 'state', 'nid']
+    fieldids = ['timeStart', 'category', 'cmd', 'result', 'state', 'nid']
     tableid = modifier.addTableForModel('system', 'job', fieldids, fieldnames, fieldvalues, filters)
     modifier.addSearchOptions('#%s' % tableid)
     modifier.addSorting('#%s' % tableid, 0, 'desc')
