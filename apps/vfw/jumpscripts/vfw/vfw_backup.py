a8cb81648113a3303f655e4eb7987f69e182f728
Reem Khamis <khamisr@codescalers.com>
2014-05-22 17:05:08 +0300

    VFW backup

---
+++ apps/vfw/jumpscripts/vfw/vfw_backup.py
@@ -0,0 +1,86 @@
+from JumpScale import j
+
+descr = """
+backup vfw config files
+"""
+
+organization = "jumpscale"
+author = "khamisr@codescalers.com"
+license = "bsd"
+version = "1.0"
+category = "vfw.backup.config"
+period = 60*60*24
+enable = True
+async = True
+roles = ["admin"]
+queue ='io'
+
+def action():
+    import JumpScale.grid.osis
+    import tarfile
+    import JumpScale.baselib.mailclient
+
+    backuppath = j.system.fs.joinPaths(j.dirs.varDir, 'vfwbackup')
+    timestamp = j.base.time.getTimeEpoch() 
+    vfwerrors = list()
+    try:
+        import JumpScale.lib.routeros
+        if j.system.fs.exists(backuppath):
+            j.system.fs.removeDirTree(backuppath)
+
+        osiscl = j.core.osis.getClient(user='root')
+        vfwcl = j.core.osis.getClientForCategory(osiscl, 'vfw', 'virtualfirewall')
+        cscl = j.core.osis.getClientForCategory(osiscl, 'cloudbroker', 'cloudspace')
+
+        routeros_password = j.application.config.get('vfw.admin.passwd')
+
+        notdestroyed = {'query': {'bool': {'must_not': [{'term': {'status': 'destroyed'}}]}}}
+        cloudspaces = cscl.simpleSearch({}, nativequery=notdestroyed)
+        cloudspaceids = [cloudspace['id'] for cloudspace in cloudspaces]
+
+        alivevfws = {'query': {'bool': {'must': [{'terms': {'domain': cloudspaceids}}]}}}
+        vfws = vfwcl.simpleSearch({}, nativequery=alivevfws)
+
+        for vfw in vfws:
+            try:
+                routeros = j.clients.routeros.get(vfw['internalip'], 'vscalers', routeros_password)
+                name = vfw['name'] or vfw['domain']
+                path = j.system.fs.joinPaths(backuppath, name)
+                routeros.backup(name, path)
+            except Exception:
+                vfwerrors.append('NAME: %(name)s      DOMAIN: %(domain)s      HOST: %(host)s      INTERNAL IP: %(internalip)s      PUBLIC IP: %(publicips)s' % vfw) 
+
+
+        #targz
+        outputpath = j.system.fs.joinPaths(j.dirs.varDir, 'vfwbackup_%s.tar.gz' % timestamp)
+        with tarfile.open(outputpath, "w:gz") as tar:
+            tar.add(backuppath)
+    except Exception:
+        import traceback
+        error = traceback.format_exc()
+        message = '''
+VFW backup at %s failed.
+Data should have been backed up to %s on the admin node.
+
+Exception:
+-----------------------------
+%s
+-----------------------------
+    ''' % (j.base.time.epoch2HRDateTime(timestamp), backuppath, error)
+        message = message.replace('\n', '<br/>')
+        j.clients.email.send('support@mothership1.com', 'smtp@incubaid.com', 'VFW backup failed', message)
+    finally:
+        if vfwerrors:
+            message = '''
+    VFW backup at %s failed.
+    Data should have been backed up to %s on the admin node.
+
+    These vfws have failed to backup:
+    -----------------------------
+    %s
+    -----------------------------
+        ''' % (j.base.time.epoch2HRDateTime(timestamp), backuppath, '<br/>'.join(vfwerrors))
+            message = message.replace('\n', '<br/>')
+            j.clients.email.send('support@mothership1.com', 'smtp@incubaid.com', 'VFW backup incomplete', message)
+        
+
