a346f6a32860bd803ac1c687ec976ebf2b2dcd19
Reem Khamis <khamisr@codescalers.com>
2014-05-22 12:22:37 +0300

    osis backup jumpscript

---
+++ apps/agentcontroller/jumpscripts/core/system/backup_osis.py
@@ -0,0 +1,58 @@
+from JumpScale import j
+
+descr = """
+check timeout jobs
+"""
+
+organization = "jumpscale"
+author = "khamisr@codescalers.com"
+license = "bsd"
+version = "1.0"
+category = "system.backup.osis"
+period = 60*60*24
+enable = True
+async = True
+roles = ["admin"]
+queue ='io'
+
+def action():
+    import JumpScale.grid.osis
+    import tarfile
+    """
+    Backs up each category under {var directory}/osisbackup/namespace/category/
+    Creates a targz of the backup under {var directory}/osisbackup_{timestamp}
+    """
+    backuppath = j.system.fs.joinPaths(j.dirs.varDir, 'osisbackup')
+    timestamp = j.base.time.getTimeEpoch() 
+    try:
+        oscl = j.core.osis.getClient(user='root')
+        namespaces = oscl.listNamespaces()
+        for namespace in namespaces:
+            categories = oscl.listNamespaceCategories(namespace)
+            for category in categories:
+                if namespace == 'system' and category == 'stats':
+                    continue
+                outputpath = j.system.fs.joinPaths(backuppath, namespace, category)
+                j.system.fs.createDir(outputpath)
+                try:
+                    oscl.export(namespace, category, outputpath)
+                except Exception:
+                    'Could not '
+
+        #targz
+        outputpath = j.system.fs.joinPaths(j.dirs.varDir, 'osisbackup_%s.tar.gz' % timestamp)
+        with tarfile.open(outputpath, "w:gz") as tar:
+            tar.add(backuppath)
+    except Exception:
+        import JumpScale.baselib.mailclient
+        import traceback
+        message = '''
+        OSIS back up at %s failed.
+        Data should have been backed up to %s on the admin node.
+
+        Exception:
+        %s
+        ''' % (j.base.time.epoch2HRDateTime(timestamp), backuppath, traceback.format_exc())
+        j.clients.email.send('support@mothership1.com', 'smtp@incubaid.com', 'OSIS backup failed', message)
+        
+
