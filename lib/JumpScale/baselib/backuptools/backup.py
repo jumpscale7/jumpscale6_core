a4ad9f840d9e179eafc6e983471c7f65d0b5014c
Hendrik Demolder <hendrik.demolder@gmail.com>
2014-06-13 17:35:28 +0200

    compress uploaded data

--- lib/JumpScale/baselib/backuptools/backup.py
+++ lib/JumpScale/baselib/backuptools/backup.py
@@ -1,5 +1,7 @@
 import ujson
 from JumpScale import j
+import lzma
+
 blocksize = 20*1024*1024
 
 def _read_file(path, block_size=0):
@@ -11,11 +13,13 @@ def _read_file(path, block_size=0):
             else:
                 return
 
-def _dump2stor(store, bucketname, data):
+def _dump2stor(store, bucketname, data, compress):
         if len(data)==0:
             return ""       
         key = j.tools.hash.md5_string(data)
         if not key in store.list_objects(bucketname):
+            if compress:
+                data = lzma.compress(data)
             store.set_object(bucketname, key, data)
         return key
 
@@ -25,19 +29,23 @@ def store_metadata(store, mdbucketname, backupname, backupmetadata):
         store.create_pool(mdbucketname)
     store.set_object(mdbucketname, backupname, ujson.dumps(backupmetadata))
 
-def backup(store, bucketname, f):
+def backup(store, bucketname, f, compress=True):
     hashes = []
     pools = store.list_pools()
     if not bucketname in pools:
         store.create_pool(bucketname)
     for data in _read_file(f, blocksize):
-        hashes.append(_dump2stor(store, bucketname, data))
+        hashes.append(_dump2stor(store, bucketname, data,compress))
     return {'path':f, 'fileparts':hashes}
 
-def restore(store, bucketname, restorepath, parts):
+def restore(store, bucketname, restorepath, parts, compress=True):
     for part in parts:
         part_content = store.get_object(bucketname, part)
-        j.system.fs.writeFile(restorepath, part_content, append=True)
+        if compress:
+            uncompressed_part_content = lzma.decompress(part_content)
+        else:
+            uncompressed_part_content = part_content
+        j.system.fs.writeFile(restorepath, uncompressed_part_content, append=True)
 
 
 
