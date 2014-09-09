df98ed189995d36405e559c12bae8149e6de424e
Jo De Boeck <deboeck.jo@gmail.com>
2014-09-09 10:37:53 +0300

    Fixes in vfw

--- apps/vfw/osis/vfw/OSIS_parent.py
+++ apps/vfw/osis/vfw/OSIS_parent.py
@@ -1,22 +1,12 @@
 from JumpScale import j
-from JumpScale.grid.osis.OSISStore import OSISStore
+from JumpScale.grid.osis.OSISStoreMongo import OSISStoreMongo
 ujson = j.db.serializers.getSerializerType('j')
 
 
-class mainclass(OSISStore):
+class mainclass(OSISStoreMongo):
 
     """
     Default object implementation
     """
-
-    def set(self, key, value, waitIndex=False):
-        if j.basetype.dictionary.check(value):
-            obj=self.getObject(value)
-        else:
-            raise RuntimeError("val should be dict in osis on set (osis_parent_vfw")
-        obj.getSetGuid()
-        self.db.set(self.dbprefix, key=obj.guid, value=ujson.dumps(obj.dump()))
-        self.index(obj.getDictForIndex())
-        return [obj.guid, True, True]
-
+    MULTIGRID = False
 
