58ac002f91eb5e2f230a4c2d0ab3f53f5118c0bd
Jo De Boeck <deboeck.jo@gmail.com>
2014-09-14 11:54:22 +0300

    Add support for ssl and replicaSet in mongodbclient

--- lib/JumpScale/grid/mongodbclient/MongoDBClient.py
+++ lib/JumpScale/grid/mongodbclient/MongoDBClient.py
@@ -1,6 +1,6 @@
 from JumpScale import j
 
-from pymongo import MongoClient
+from pymongo import MongoClient, MongoReplicaSetClient
 
 class MongoDBClient:
 
@@ -12,3 +12,21 @@ class MongoDBClient:
         else:
             return client
 
+    def getByInstance(self, instancename):
+        jp=j.packages.findNewest(name="mongodb_client",domain='jumpscale')
+        jp=jp.load(instancename)
+        hrd = jp.hrd_instance
+        if hrd is None:
+            j.events.opserror_critical("Could not find mongodb_client for instance %s" % instancename)
+        ipaddr = hrd.get("mongodb.client.addr")
+        port = hrd.getInt("mongodb.client.port")    
+        ssl = False
+        if j.application.config.exists('mongodb.client.ssl'):
+            ssl = j.application.config.getBool('mongodb.client.ssl')
+        replicaset = ""
+        if j.application.config.exists('mongodb.client.replicaset'):
+            replicaset = j.application.config.get('mongodb.client.replicaset')
+        if replicaset == "":
+            return MongoClient(host=ipaddr, port=port, ssl=ssl)
+        else:
+            return MongoReplicaSetClient(host=ipaddr, port=port, ssl=ssl, replicaSet=replicaset)
