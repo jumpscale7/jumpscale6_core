c1201ef32e78a1cca681b7967877f54dfe1923ff
Mohammad Tayseer <m.tayseer82@gmail.com>
2014-09-18 18:39:43 +0300

    Cleanup of finder macro code

--- apps/portalbase/macros/page/finder/1_finder.py
+++ apps/portalbase/macros/page/finder/1_finder.py
@@ -3,17 +3,17 @@ import os
 def main(j, args, params, tags, tasklet):
     params.result = page = args.page
 
-    search_space = args.paramsExtra.get('space')
+    keywords = args.expandParamsAsDict()
+    search_space = keywords.get('space', args.doc.getSpaceName())
     space = j.core.portal.active.spacesloader.spaces.get(search_space, None)
     if not space:
         page.addMessage('ERROR: space {} does not exist'.format(search_space))
         return params
 
     space_path = space.model.path
-    search_term = args.paramsExtra.get('search_term') or ''
+    search_term = keywords.get('search_term') or keywords.get('text') or ''
 
     if not search_term:
-        page.addMessage('ERROR: enter a search term')
         return params
 
 
@@ -22,26 +22,6 @@ def main(j, args, params, tags, tasklet):
         for f in files:
             if f.endswith('.wiki') and '.space' not in root and search_term.lower() in open(os.path.join(root, f)).read().lower():
                 result_files.append(os.path.join(root, f))
-
-    page.addMessage('''
-        <form class="form-horizontal" role="form" method=get target=".">
-          <div class="control-group">
-            <label class="control-label" for="search_term">Search term</label>
-            <div class="controls">
-                <input type="text" class="form-control" name="search_term" placeholder="What are you searching for?" value="{}">
-            </div>
-          </div>
-          <div class="control-group">
-            <label class="control-label" for="space">Space</label>
-            <div class="controls">
-                <input type="text" class="form-control" name="space" placeholder="Space you are searching in" value="{}">
-            </div>
-          </div>
-
-          <input type=submit value="Submit" class="btn btn-default" value="Search" />
-        </form>
-
-    '''.format(search_term, search_space))
 
     if result_files:
         page.addMessage('<dl><dt>Found in {} file(s)</dt><dd><ul class="col3">'.format(len(result_files)))
