dc07e173b540319a77400b4bd25f9bb44346994f
Jo De Boeck <deboeck.jo@gmail.com>
2014-09-30 14:04:20 +0200

    Fixes in machine visibility

--- apps/gridportal/base/Grid/.macros/wiki/machine/1_machine.py
+++ apps/gridportal/base/Grid/.macros/wiki/machine/1_machine.py
@@ -2,18 +2,19 @@ import datetime
 
 def main(j, args, params, tags, tasklet):
     id = args.getTag('id')
+    gid = args.getTag('gid')
     if not id:
         out = 'Missing machine id param "id"'
         params.result = (out, args.doc)
         return params
 
-    machines = j.apps.system.gridmanager.getMachines(id=id)
-    if not machines:
+    machine = j.core.portal.active.osis.get('system', 'machine', "%s_%s" % (gid, id))
+    if not machine:
         params.result = ('Machine with id %s not found' % id, args.doc)
         return params
 
     def objFetchManipulate(id):
-        obj = machines[0]
+        obj = machine
         for attr in ['roles', 'ipaddr']:
             obj[attr] = ', '.join([str(x) for x in obj[attr]]) 
 
