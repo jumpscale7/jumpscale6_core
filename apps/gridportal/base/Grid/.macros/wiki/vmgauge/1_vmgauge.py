5a5f2274f4ea4fbd56b3b87606e93bc1fb346424
Jo De Boeck <deboeck.jo@gmail.com>
2014-10-12 11:39:44 +0200

    Gridportal: Fix dashboard

--- apps/gridportal/base/Grid/.macros/wiki/vmgauge/1_vmgauge.py
+++ apps/gridportal/base/Grid/.macros/wiki/vmgauge/1_vmgauge.py
@@ -8,7 +8,7 @@ def main(j, args, params, tags, tasklet):
     result = "{{jgauge width:%(width)s id:%(id)s height:%(height)s val:%(running)s start:0 end:%(total)s}}"
     cl = j.core.osis.getClient(user='root')
     vmcl = j.core.osis.getClientForCategory(cl, 'system', 'machine')
-    running = len(vmcl.simpleSearch({'state': 'RUNNING'}))
+    running = vmcl.search({'state': 'RUNNING'})[0]
     total = len(vmcl.list())
     result = result % {'height': height,
                        'width': width,
