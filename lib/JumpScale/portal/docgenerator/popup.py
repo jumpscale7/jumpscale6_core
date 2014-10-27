9c83c883ed8216e35b96bad023adc96039e38e2c
Mohammad Tayseer <m.tayseer82@gmail.com>
2014-10-27 14:15:25 +0200

    Changed popup header to not make a conflict with toc macro

--- lib/JumpScale/portal/docgenerator/popup.py
+++ lib/JumpScale/portal/docgenerator/popup.py
@@ -98,7 +98,7 @@ class Popup(object):
             <div id="${id}" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="${id}Label" aria-hidden="true">
               <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
-                <h3 id="${id}Label">${header}</h3>
+                <div id="${id}Label" class="modal-header-text">${header}</div>
               </div>
               <div class="modal-body modal-body-sending">
                 Sending...
@@ -120,7 +120,7 @@ class Popup(object):
         content = template.render(id=self.id, header=self.header, action_button=self.action_button, form_layout=self.form_layout, 
                                 widgets=self.widgets, submit_url=self.submit_url)
 
-        css = '.modal-body-sending, .modal-body-error { display: none }'
+        css = '.modal-body-sending, .modal-body-error { display: none } .modal-header-text { font-weight: bold; font-size: 24.5px; line-height: 30px; }'
         if css not in page.head:
             page.addCSS(cssContent=css)
 
