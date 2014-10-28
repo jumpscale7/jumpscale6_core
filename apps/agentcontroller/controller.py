7382ee8b2b9fd48a10c33aabc9ccab1e95f74066
Jo De Boeck <deboeck.jo@gmail.com>
2014-10-28 17:07:34 +0200

    Dont crash if role is not running

--- apps/agentcontroller/controller.py
+++ apps/agentcontroller/controller.py
@@ -408,7 +408,8 @@ class ControllerCMDS():
             j.events.inputerror_critical("executeJumpScript requires either nid and gid or role")
         def noWork():
             sessionid = session.id
-            job=self.jobclient.new(sessionid=sessionid,gid=gid,nid=nid,category=organization,cmd=name,queue=queue,args=args,log=True,timeout=timeout,roles=[role],wait=wait,errorreport=errorreport)
+            ngid = gid or j.application.whoAmI.gid
+            job=self.jobclient.new(sessionid=sessionid,gid=ngid,nid=nid,category=organization,cmd=name,queue=queue,args=args,log=True,timeout=timeout,roles=[role],wait=wait,errorreport=errorreport)
             self._log("nothingtodo")
             job.state="NOWORK"
             job.timeStop=job.timeStart
