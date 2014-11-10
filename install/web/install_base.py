11dcbd9b6d270fa2b3e43660310ac157e927bc99
despiegk <kristof@incubaid.com>
2014-11-10 16:12:31 +0200

    random add

--- install/web/install_base.py
+++ install/web/install_base.py
@@ -1,7 +1,8 @@
 
 #BOOTSTRAP CODE
 from urllib import urlopen
-handle = urlopen("https://raw.githubusercontent.com/Jumpscale/jumpscale_core/master/install/InstallTools.py")
+import random
+handle = urlopen("https://raw.githubusercontent.com/Jumpscale/jumpscale_core/master/install/InstallTools.py?%s"%random.randint(1, 10000000))
 exec(handle.read())
 
 print do.prepareUbuntu14Development(js=True)
