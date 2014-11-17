33c5fc26138c7727b8d6ebcbbb4328c3eb434ae4
Karim Ali <karim.ali.dev@gmail.com>
2014-11-17 16:30:42 +0200

    Eve grid macro updates

--- apps/portalbase/macros/page/eveGrid/1_evegrid.py
+++ apps/portalbase/macros/page/eveGrid/1_evegrid.py
@@ -1,14 +1,31 @@
+from itertools import count
+import json
 
 def main(j, args, params, tags, tasklet):
 	page = args.page
+	
+	hrd = j.core.hrd.getHRD(content=args.cmdstr)
 
-	pars = (line.split('=') for line in args.cmdstr.strip().splitlines())
-	pars = dict((p[0].strip(), p[1].strip()) for p in pars)
 	eveGrid = {
-		'specJsonPath': pars.get('spec.json.path', '/docs/spec.json'),
-		'schemaURL': pars.get('schema.url', ''),
-		'entityName': pars.get('entity.name', '')
+		'specJsonPath': hrd.get('spec_json_path', default='/docs/spec.json'),
+		'schemaURL': hrd.get('schema_url', default=''),
+		'entityName': hrd.get('entity_name', default=''),
+		'datetimeFields': hrd.get('datetime_fields', default=''),
 	}
+
+	eveGrid['columns'] = []
+	for i in count(1):
+		column = {}
+		column['data'] = hrd.get('column.{}.data'.format(i), default='')
+		if not column['data']:
+			break
+		column['header'] = hrd.get('column.{}.header'.format(i), default='')
+		column['format'] = hrd.get('column.{}.format'.format(i), default='')
+		eveGrid['columns'].append(column)
+	
+	# import ipdb; ipdb.set_trace()
+	eveGrid['columns'] = (json.dumps(eveGrid['columns']))
+
 
 	# Add our static resources only once to the page
 	if '/jslib/jquery/jqueryDataTable/css/eve-grid.css' not in str(page):
@@ -16,21 +33,26 @@ def main(j, args, params, tags, tasklet):
 		page.addCSS('/jslib/jquery/jqueryDataTable/css/dataTables.bootstrap.css')
 		page.addCSS('/jslib/jquery/jqueryDataTable/css/bootstrap-theme.min.css')
 		page.addCSS('/jslib/jquery/jqueryDataTable/css/eve-grid.css')
+		page.addCSS('/jslib/jquery/jquery-ui.structure.min.css')
+		page.addCSS('//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.theme.min.css')
 
 		page.addJS('/jslib/jquery/jqueryDataTable/js/jquery.dataTables.js')
 		page.addJS('/jslib/angular/angular1-3-0.min.js')
 		page.addJS('/jslib/bootstrap/js/bootstrap.min.js')
 		page.addJS('/jslib/underscore/underscore-min.js')
 		page.addJS('/jslib/jquery/jqueryDataTable/js/dataTables.bootstrap.js')
+		page.addJS('/jslib/moment.js')
+		page.addJS('/jslib/jquery/jquery-ui.min.js')
+		page.addJS('/jslib/jquery/jquery-ui-sliderAccess.js')		
+		page.addJS('/jslib/jquery/jquery-ui-timepicker-addon.min.js')
 		page.addJS('/jslib/jquery/jqueryDataTable/js/eve-grid.js')
-		
+	
 	page.addMessage('''
-		<div class="container">
-	        <div id="{entityName}-container" eve-grid eve-url="{schemaURL}" eve-entity="{entityName}" eve-spec-path="{specJsonPath}">
+		<div class="container eve-grid-container">
+	        <div id="{entityName}-container" eve-grid eve-url="{schemaURL}" eve-entity="{entityName}" eve-spec-path="{specJsonPath}" datetime-fields={datetimeFields} columns='{columns}'>
 	       	</div>
     	</div>
 	 '''.format(**eveGrid))
-
 	params.result = page
 	return params
 
