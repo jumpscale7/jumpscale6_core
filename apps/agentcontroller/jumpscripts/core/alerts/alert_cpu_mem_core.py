f0f903b692945bd13b22093d6549ebe4e4b66ab0
Jo De Boeck <deboeck.jo@gmail.com>
2014-11-18 16:53:48 +0200

    Alter cpu alert to also do mem alert

--- apps/agentcontroller/jumpscripts/core/alerts/alert_cpu_mem_core.py
+++ apps/agentcontroller/jumpscripts/core/alerts/alert_cpu_mem_core.py
@@ -21,21 +21,24 @@ roles = ['master']
 
 def action():
     osis = j.core.osis.client
-    results = osis.search('system', 'stats', 'select mean(value) from /stats.gauges.\d+_\d+_cpu.promile.*/ where time > now() - 1h;')
+    results = osis.search('system', 'stats', 'select mean(value) from /stats.gauges.\d+_\d+_(cpu.promile|memory.percent).*/ where time > now() - 1h;')
     for noderesult in results:
-        series = noderesult['name']
-        avgcpu = noderesult['points'][0][-1]/10.
+        parts = noderesult['name'].split('.')[2]
+        value = noderesult['points'][0][-1]
+        gid, nid, type = parts.split('_')
+        gid = int(gid)
+        nid = int(nid)
+        if type == 'cpu':
+            value /= 10.
+
         level = None
-        if avgcpu > 95:
+        if value > 95:
             level = 1
-        elif avgcpu > 80:
+        elif value > 80:
             level = 2
         if level:
             #stats.gauges.500_6_cpu.promile
-            gid, nid = series.split('.')[2].split('_')[0:2]
-            gid = int(gid)
-            nid = int(nid)
-            msg = 'CPU load above treshhold value last hour avergage is %s' % avgcpu
+            msg = '%s load above treshhold value last hour avergage is %s' % (type.upper(), value)
             eco = j.errorconditionhandler.getErrorConditionObject(msg=msg, category='monitoring', level=level, type='OPERATIONS')
             eco.nid = nid
             eco.gid = gid
