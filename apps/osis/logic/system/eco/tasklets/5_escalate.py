0df452378ecd83f51ad004be960c194673330cd1
Jo De Boeck <deboeck.jo@gmail.com>
2014-12-21 09:54:14 +0200

    Fix issues in transformation of eco to alert

--- apps/osis/logic/system/eco/tasklets/5_escalate.py
+++ apps/osis/logic/system/eco/tasklets/5_escalate.py
@@ -7,25 +7,30 @@ def main(j, params, service, tags, tasklet):
     eco = params.value
     session = params.session
     alertservice = j.core.osis.cmds._getOsisInstanceForCat('system', 'alert')
-    
+
     alerts = alertservice.search({'eco':eco['guid']}, session=session)[1:]
     alert = {'eco': eco['guid'],
                 'errormessage': eco['errormessage'],
                 'errormessagePub': eco['errormessagePub'],
                 'category': eco['category'],
-                'lasttime': eco['epoch']}
+                'gid': eco['gid'],
+                'nid': eco['nid'],
+                'lasttime': eco['lasttime']}
     if not alerts:
         alert['inittime'] = eco['epoch']
-        alert['description'] = eco['description']
         alert['state'] = 'ALERT'
-        alert['descriptionpub'] = eco['descriptionpub']
+        alert['level'] = 1
+        alertobj = alertservice.new()
+        alertobj.load(alert)
+        alert = alertobj.dump()
+
     else:
         alertdata = alerts[0]
         if alertdata['state'] in ['RESOLVED', 'CLOSED']:
             alertdata['state'] = 'ALERT'
-        alertdata.update(alert)		
+        alertdata.update(alert)
         alert = alertdata
-    alertservice.set(alert, session=session)
+    alertservice.set(None, alert, session=session)
     
 def match(j, params, service, tags, tasklet):
     return params.action == 'set'
