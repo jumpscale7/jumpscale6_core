a157eb3d90c359ad8620bad6ad17cfda0362ddec
christophe dCPM <martinsc@mothership1.com>
2014-12-23 10:18:52 +0100

    allow patials on integer

--- lib/JumpScale/grid/osis/OSISClientForCat.py
+++ lib/JumpScale/grid/osis/OSISClientForCat.py
@@ -151,6 +151,7 @@ class OSISClientForCat():
             query = nativequery.copy()
         else:
             query = {'query': {'bool': {'must': list()}}}
+        query.setdefault('query', {}).setdefault('bool', {}).setdefault('must', [])
         myranges = {}
         for k, v in params.iteritems():
             if isinstance(v, dict):
@@ -166,10 +167,8 @@ class OSISClientForCat():
                 term = {'term': {k: v}}
                 query['query']['bool']['must'].append(term)
         for key, value in myranges.iteritems():
-            query.setdefault('query', {}).setdefault('bool', {}).setdefault('must', [])
             query['query']['bool']['must'].append({'range': {key: value}})
         if partials:
-            query.setdefault('query', {}).setdefault('bool', {}).setdefault('must', [])
             query['query']['bool']['must'].append({'wildcard': partials})
         boolq = query.get('query', {}).get('bool', {})
         def isEmpty(inputquery):
